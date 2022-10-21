35cf425388e8b3c430bcd956e8a875c921d0f92e
strongcookdas <goodjav141592@gmail.com>
2022-10-21 10:23:49 +0900

    feat : String split 메소드로 괄호 풀기

--- src/main/java/week3/algorithm_1021/stack/SolveBracket.java
+++ src/main/java/week3/algorithm_1021/stack/SolveBracket.java
@@ -1,12 +1,25 @@
 package week3.algorithm_1021.stack;
 
 import java.util.EmptyStackException;
+import java.util.Stack;
 
 public class SolveBracket {
 
+    Stack<String> stack = new Stack<>();
+
+    /*
     public boolean solution(String s) {
         while (s.indexOf("()") != -1) {
             s = s.replace("()", "");
+        }
+        return s.length()==0;
+    }
+     */
+
+    public boolean solution(String s) {
+        while (s.indexOf("()") != -1) {
+            String[] tmp = s.split("\\(\\)");
+            s = String.join("",tmp);
         }
         return s.length()==0;
     }

b4e66b4347153e2b1a981499ed43a58349c160c1
strongcookdas <goodjav141592@gmail.com>
2022-10-21 10:33:24 +0900

    feat : stack으로 괄호 풀기

--- src/main/java/week3/algorithm_1021/stack/SolveBracket.java
+++ src/main/java/week3/algorithm_1021/stack/SolveBracket.java
@@ -16,11 +16,29 @@ public class SolveBracket {
     }
      */
 
+    /*
     public boolean solution(String s) {
         while (s.indexOf("()") != -1) {
             String[] tmp = s.split("\\(\\)");
             s = String.join("",tmp);
         }
         return s.length()==0;
+    }
+     */
+
+    public boolean solution(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)=='('){
+                stack.push("(");
+            }
+
+            if(s.charAt(i)==')'){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.size()==0;
     }
 }

48739bc866437003a57045d5f4f6c3044a62214d
strongcookdas <goodjav141592@gmail.com>
2022-10-24 09:50:38 +0900

    algorithm : 프로그래머스 k번째 수 풀기

---
+++ src/main/java/week4/algorithm_1024/sort/KthTest.java
@@ -0,0 +1,29 @@
+package week4.algorithm_1024.sort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class KthTest {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int idx = 0;
+
+        for (int[] command : commands) {
+            int[] tmp = Arrays.copyOfRange(array, command[0]-1, command[1]);
+            Arrays.sort(tmp);
+            answer[idx++] = tmp[command[2]-1];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+
+        KthTest k = new KthTest();
+
+        int[] ans = k.solution(arr, commands);
+        System.out.println(Arrays.toString(ans));
+    }
+}

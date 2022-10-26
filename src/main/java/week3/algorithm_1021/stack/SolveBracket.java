ec941f5498cf047e06a26706d321e15839358cd5
strongcookdas <goodjav141592@gmail.com>
2022-10-26 10:42:15 +0900

    algorithm : 괄호 풀기

--- src/main/java/week3/algorithm_1021/stack/SolveBracket.java
+++ src/main/java/week3/algorithm_1021/stack/SolveBracket.java
@@ -5,28 +5,27 @@ import java.util.Stack;
 
 public class SolveBracket {
 
-    Stack<String> stack = new Stack<>();
 
-    /*
-    public boolean solution(String s) {
+    public boolean solution1(String s) {
         while (s.indexOf("()") != -1) {
             s = s.replace("()", "");
         }
         return s.length()==0;
     }
-     */
 
-    /*
-    public boolean solution(String s) {
+
+
+    public boolean solution2(String s) {
         while (s.indexOf("()") != -1) {
             String[] tmp = s.split("\\(\\)");
             s = String.join("",tmp);
         }
         return s.length()==0;
     }
-     */
 
-    public boolean solution(String s) {
+
+    public boolean solution3(String s) {
+        Stack<String> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             if(s.charAt(i)=='('){
                 stack.push("(");
@@ -41,4 +40,21 @@ public class SolveBracket {
         }
         return stack.size()==0;
     }
+    public boolean solution(String s) {
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)=='('){
+                cnt++;
+            }
+
+            if(s.charAt(i)==')'){
+                if(cnt<1){
+                    return false;
+                }
+                cnt--;
+            }
+        }
+        return cnt ==0;
+    }
+
 }

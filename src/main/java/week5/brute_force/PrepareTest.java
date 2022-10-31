7187b0391b6c440c9bbd9fe84c54ffd8d8eb8915
strongcookdas <goodjav141592@gmail.com>
2022-10-31 09:37:59 +0900

    algorithm : 프로그래머스_모의고사

---
+++ src/main/java/week5/brute_force/PrepareTest.java
@@ -0,0 +1,22 @@
+package week5.brute_force;
+
+//1. 3명의 student 학생의 답안 형식을 배열로 선언
+//2. 2중 for문 사용 3번 반복하고 답안의 길이만금 비교
+//3. 맞으면 count++해준다.
+//4. count한 것을 answer 배열에 저장
+//6. answer 배열은 객체배열 (student 번호, count 필드가 있는 객체)
+//7. count를 기준으로 배열 정렬
+//8. 정렬된 학생 번호는 1차원 배열에 다시 저장
+
+public class PrepareTest {
+    public int[] solution(int[] answer){
+        int[] student1 = {1,2,3,4,5};
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        PrepareTest test = new PrepareTest();
+        System.out.println();
+    }
+}

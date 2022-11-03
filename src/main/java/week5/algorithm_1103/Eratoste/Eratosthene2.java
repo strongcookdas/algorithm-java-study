bf65e7012da4eacb28ee8fa533b68ee4f2b94401
strongcookdas <goodjav141592@gmail.com>
2022-11-03 10:01:32 +0900

    algorithm : 나머지 없는 에라토스테네의 체

---
+++ src/main/java/week5/algorithm_1103/Eratoste/Eratosthene2.java
@@ -0,0 +1,28 @@
+package week5.algorithm_1103.Eratoste;
+
+import java.util.Arrays;
+
+public class Eratosthene2 {
+
+    public int solution(int N){
+        boolean nums[] = new boolean[N+1];
+        Arrays.fill(nums,true);
+        for (int i = 2; i*i <= N ; i++) {
+            for (int j = i+i; j <= N; j+=i) {
+                nums[j]=false;
+            }
+        }
+
+        int cnt =0;
+        for (int i = 2; i < N+1; i++) {
+            if(nums[i]==true)
+                cnt++;
+        }
+
+        return cnt;
+    }
+    public static void main(String[] args) {
+        Eratosthene2 e = new Eratosthene2();
+        System.out.println(e.solution(5));
+    }
+}

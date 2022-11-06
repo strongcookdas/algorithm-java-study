0dd1d4b399a74b89f8053405f64f672f7efc82be
strongcookdas <goodjav141592@gmail.com>
2022-11-06 16:48:46 +0900

    algorithm : 프로그래머스_완주하지 못한 선수

--- src/main/java/week4/algorithm_1027/hash/AlthletesTest.java
+++ src/main/java/week4/algorithm_1027/hash/AlthletesTest.java
@@ -5,6 +5,17 @@ import java.util.Map;
 
 public class AlthletesTest {
 
+    /*
+    설계
+    1. HashMap에 모든 Participant를 put한다.
+        a. key에 해당하는 값이 이미 존재한다면 그 값에 1을 더하도록 한다.
+        b. key에 값이 없다면, 0으로 초기화 후 1을 더해주고, Map에 put 해준다.
+
+    2. HashMap에 모든 Completion, 완주자를 put한다.
+        a. 1을 빼준다.
+
+    3. HashMap의 값이 0이 아닌 key를 반환
+     */
     public String solution(String[] participant, String[] completion){
         Map<String, Integer> memo = new HashMap<>();
         for(int i = 0; i < participant.length; i++){

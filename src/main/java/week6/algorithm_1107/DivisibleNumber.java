45524b63c6781ead24ecea8608d68198de085e03
strongcookdas <goodjav141592@gmail.com>
2022-11-07 10:25:36 +0900

    algorithm : 프로그래머스_나누어 떨어지는 수_완료

--- src/main/java/week6/algorithm_1107/DivisibleNumber.java
+++ src/main/java/week6/algorithm_1107/DivisibleNumber.java
@@ -7,26 +7,26 @@ import java.util.Collections;
 public class DivisibleNumber {
     public int[] solution(int[] arr, int divisor) {
         ArrayList<Integer> ans = new ArrayList<>();
-        for (int num:arr) {
-            if(num%divisor==0)
+        for (int num : arr) {
+            if (num % divisor == 0)
                 ans.add(num);
         }
-        if(ans.size()==0){
-            int[] answer ={-1};
+        if (ans.size() == 0) {
+            int[] answer = {-1};
             return answer;
         }
         Collections.sort(ans);
         int[] answer = new int[ans.size()];
         for (int i = 0; i < ans.size(); i++) {
-            answer[i]=ans.get(i);
+            answer[i] = ans.get(i);
         }
         return answer;
     }
 
     public static void main(String[] args) {
         DivisibleNumber d = new DivisibleNumber();
-        int[] arr = {5,9,7,10};
+        int[] arr = {5, 9, 7, 10};
         int divisor = 5;
-        System.out.println(Arrays.toString(d.solution(arr,divisor)));
+        System.out.println(Arrays.toString(d.solution(arr, divisor)));
     }
 }

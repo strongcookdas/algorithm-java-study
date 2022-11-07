cb12574990914a8870e21809989fdaa9c2c3b1e1
strongcookdas <goodjav141592@gmail.com>
2022-11-07 10:26:45 +0900

    알고리즘:프로그래머스_하샤드 수_완료

--- src/main/java/week6/algorithm_1107/Hashad.java
+++ src/main/java/week6/algorithm_1107/Hashad.java
@@ -1,14 +1,14 @@
 package week6.algorithm_1107;
 
 public class Hashad {
-    public boolean solution(int num){
-        int sum=0;
+    public boolean solution(int num) {
+        int sum = 0;
         int tmp = num;
-        while(tmp>0){
-            sum+=(tmp%10);
-            tmp/=10;
+        while (tmp > 0) {
+            sum += (tmp % 10);
+            tmp /= 10;
         }
-        if(num%sum==0)
+        if (num % sum == 0)
             return true;
         return false;
     }

ba49caa86149d599e778a3675c5e423a6aa24e03
strongcookdas <goodjav141592@gmail.com>
2022-11-08 10:24:23 +0900

    algorithm : 코드업_BinarySearch_진행중

---
+++ src/main/java/week6/algorithm_1108/BinarySearch.java
@@ -0,0 +1,33 @@
+package week6.algorithm_1108;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+    public int solution(int[] arr, int target){
+        int startIdx=0;
+        int endIdx=arr.length-1;
+        int ans=-1;
+        int midIdx = arr.length/2;
+        while(endIdx!=startIdx){
+            if(target>arr[midIdx])
+                startIdx = midIdx+1;
+            else
+                endIdx = midIdx-1;
+
+            midIdx = startIdx + (endIdx-startIdx+1)/2;
+        }
+        return (arr[midIdx]==target)? midIdx+1:-1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        int target = sc.nextInt();
+        int[] arr = new int[cnt];
+        int idx = 0;
+        while(cnt--!=0){
+            arr[idx++]=sc.nextInt();
+        }
+        System.out.println(new BinarySearch().solution(arr,target));
+    }
+}

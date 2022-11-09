c557256b034b7c88cca94b383e05aa11496d73cd
strongcookdas <goodjav141592@gmail.com>
2022-11-09 09:30:51 +0900

    algorithm : SelectionSort 완료

---
+++ src/main/java/week6/algorithm_1109/SelectionSort.java
@@ -0,0 +1,31 @@
+package week6.algorithm_1109;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public void swap(int[] arr, int i, int j){
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public int[] solution(int[] arr){
+        int minidx;
+        for (int i = 0; i < arr.length; i++) {
+            minidx = i;
+            for (int j = i; j < arr.length; j++) {
+                if(arr[j]<arr[minidx]){
+                    minidx=j;
+                }
+            }
+            swap(arr,i,minidx);
+        }
+        System.out.println(Arrays.toString(arr));
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,7,4,9,10,223,111,23,3,39};
+    }
+}

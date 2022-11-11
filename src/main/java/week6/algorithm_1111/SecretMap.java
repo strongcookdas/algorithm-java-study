88b211c838c5bfd23a257333ea59a2f193dcb09e
strongcookdas <goodjav141592@gmail.com>
2022-11-11 09:14:30 +0900

    algorithm : 프로그래머스_[1차]비밀지도_설계

---
+++ src/main/java/week6/algorithm_1111/SecretMap.java
@@ -0,0 +1,12 @@
+package week6.algorithm_1111;
+/*
+1. 2차원 배열을 1개 만든다
+2. 매개변수로 넘어온 숫자열 배열의 수를 or 연산한다.
+3. 연산한 결과가 1이면 벽 0이면 공백을 2차원 배열에 입력한다.
+ */
+public class SecretMap {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+        return answer;
+    }
+}

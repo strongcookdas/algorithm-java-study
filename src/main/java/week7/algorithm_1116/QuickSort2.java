0f85054bd57e85314156c0c8c6f42dfa3a64a704
strongcookdas <goodjav141592@gmail.com>
2022-11-18 09:05:21 +0900

    algorithm : 배열을 활용한 퀵소트 구현

--- src/main/java/week7/algorithm_1116/QuickSort2.java
+++ src/main/java/week7/algorithm_1116/QuickSort2.java
@@ -10,25 +10,28 @@ public class QuickSort2 {
         arr[j] = temp;
     }
 
-    public int[] solution(int[] arr, int start, int end) {
+    public void solution(int[] arr, int start, int end) {
+        if (start == end) return;
 
-        int pivot = (start + end) / 2;
+        int pivot = arr[arr.length / 2];
 
-        while (start < end) {
-            if (arr[pivot] < arr[start] && arr[pivot] > arr[end])
-                swap(arr, start, end);
-            if (arr[start] < arr[pivot]) start++;
-            if (arr[end] > arr[pivot]) end--;
-            System.out.printf("%d %d\n", start, end);
-            System.out.println(Arrays.toString(arr));
+        while (start <= end) {
+            while (arr[start] < pivot) start++;
+            while (arr[end] > pivot) end--;
+            //start보다 end가 작은 상황일 때 교환할 경우는 없다.
+            //pivot에서 start와 end가 모이는 구조이기 때문이다.
+            //교환이 다 이루어진 이후의 start와 end는 pivot을 가리키고 있을 것이다.
+            swap(arr, start, end);
+            start++;
+            end--;
+
+            System.out.printf(Arrays.toString(arr));
         }
-
-        return arr;
     }
 
     public static void main(String[] args) {
         QuickSort2 q = new QuickSort2();
         int[] arr = {20, 18, 5, 19, 40, 50, 5, 25};
-        System.out.println(Arrays.toString(q.solution(arr, 0, arr.length - 1)));
+        q.solution(arr, 0, arr.length - 1);
     }
 }

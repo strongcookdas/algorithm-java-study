092b67238138ba95ba2cc9635bb430fcb64405de
strongcookdas <goodjav141592@gmail.com>
2022-11-18 10:10:28 +0900

    algorithm : 프로그래머스_시저암호 완료

---
+++ src/main/java/week7/algorithm_1118/CaesarCipher.java
@@ -0,0 +1,35 @@
+package week7.algorithm_1118;
+
+public class CaesarCipher {
+    public String solution(String s, int n) {
+
+        String ans = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
+                int word = (char) (s.charAt(i) + n);
+                if (word > 'z') {
+                    word = 'a' + (word - 'z') - 1;
+                }
+                ans += (char) word;
+            } else if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                int word = (char) (s.charAt(i) + n);
+                if (word > 'Z') {
+                    word = 'A' + (word - 'Z') - 1;
+                }
+                ans += (char) word;
+            } else {
+                ans += s.charAt(i);
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        CaesarCipher c = new CaesarCipher();
+
+        System.out.println(c.solution("aBz", 4));
+    }
+}

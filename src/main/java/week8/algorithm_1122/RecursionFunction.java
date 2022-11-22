a9097a9ecc8758b0ad0973c2567f1f778d05b49e
strongcookdas <goodjav141592@gmail.com>
2022-11-22 09:26:15 +0900

    algorithm : 재귀_숫자 출력 완료

---
+++ src/main/java/week8/algorithm_1122/RecursionFunction.java
@@ -0,0 +1,14 @@
+package week8.algorithm_1122;
+
+public class RecursionFunction {
+    public void solution(int n){
+        if(n==-1) return;
+        System.out.println(n);
+        solution(--n);
+    }
+
+    public static void main(String[] args) {
+        RecursionFunction r = new RecursionFunction();
+        r.solution(10);
+    }
+}

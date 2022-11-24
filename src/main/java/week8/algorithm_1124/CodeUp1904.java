1574c1e2839a5b651932c29c30e788689d226fa3
strongcookdas <goodjav141592@gmail.com>
2022-11-24 09:59:50 +0900

    algorithm : 코드업 홀수 출력하기 재귀

---
+++ src/main/java/week8/algorithm_1124/CodeUp1904.java
@@ -0,0 +1,17 @@
+package week8.algorithm_1124;
+
+import java.util.Scanner;
+
+public class CodeUp1904 {
+    public void solution(int num, int repeat){
+        if(num>repeat) return;
+        if(num%2!=0)
+            System.out.print(num+" ");
+        solution(++num,repeat);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        new CodeUp1904().solution(sc.nextInt(),sc.nextInt());
+    }
+}

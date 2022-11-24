d8a79bbc3229c20c341c7b80afeb2656e66c0399
strongcookdas <goodjav141592@gmail.com>
2022-11-24 09:46:06 +0900

    algorithm : 코드업 계단 오르기 재귀 완료

---
+++ src/main/java/week8/algorithm_1123/CodeUp1856.java
@@ -0,0 +1,17 @@
+package week8.algorithm_1123;
+
+import java.util.Scanner;
+
+public class CodeUp1856 {
+    public int solution (int n){
+        if(n==1) return 1;
+        else if(n==2) return 2;
+        else if(n==3) return 4;
+        return solution(n-1)+solution(n-2)+solution(n-3);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(new CodeUp1856().solution(sc.nextInt()));
+    }
+}

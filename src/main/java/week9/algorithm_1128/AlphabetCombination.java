b0bd5175b19c96668f078219359686666183a74d
strongcookdas <goodjav141592@gmail.com>
2022-11-28 09:19:12 +0900

    algorithm : 알파벳 2개의 모든 경우의 수 출력

--- src/main/java/week9/algorithm_1128/AlphabetCombination.java
+++ src/main/java/week9/algorithm_1128/AlphabetCombination.java
@@ -1,10 +1,10 @@
 package week9.algorithm_1128;
 
 public class AlphabetCombination {
-    public void solution(){
+    public void solution() {
         for (int i = 0; i < 26; i++) {
             for (int j = 0; j < 26; j++) {
-                System.out.println((char)('a'+i)+" "+(char)('a'+j));
+                System.out.println((char) ('a' + i) + " " + (char) ('a' + j));
             }
         }
     }

966e27e71580ed6331df5d3199294bbf66e75ae8
strongcookdas <goodjav141592@gmail.com>
2022-11-28 09:34:57 +0900

    algorithm : 재귀로 모든 알파벳 출력

--- src/main/java/week9/algorithm_1128/AlphabetCombination.java
+++ src/main/java/week9/algorithm_1128/AlphabetCombination.java
@@ -1,15 +1,21 @@
 package week9.algorithm_1128;
 
 public class AlphabetCombination {
-    public void solution() {
-        for (int i = 0; i < 26; i++) {
-            for (int j = 0; j < 26; j++) {
-                System.out.println((char) ('a' + i) + " " + (char) ('a' + j));
-            }
-        }
+//    public void solution() {
+//        for (int i = 0; i < 26; i++) {
+//            for (int j = 0; j < 26; j++) {
+//                System.out.println((char) ('a' + i) + " " + (char) ('a' + j));
+//            }
+//        }
+//    }
+
+    public void solution(int c){
+        System.out.println((char)c);
+        if(c=='z') return;
+        solution(c+1);
     }
 
     public static void main(String[] args) {
-        new AlphabetCombination().solution();
+        new AlphabetCombination().solution('a');
     }
 }

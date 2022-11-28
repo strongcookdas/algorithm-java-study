0f729bbf5bfebbcdbb5c38fb2bc3043ff6abecfa
strongcookdas <goodjav141592@gmail.com>
2022-11-28 10:15:13 +0900

    algorithm : 재귀로 모든 알파벳 출력 (2자리)

--- src/main/java/week9/algorithm_1128/AlphabetCombination.java
+++ src/main/java/week9/algorithm_1128/AlphabetCombination.java
@@ -9,10 +9,22 @@ public class AlphabetCombination {
 //        }
 //    }
 
-    public void solution(int c){
-        System.out.println((char)c);
-        if(c=='z') return;
-        solution(c+1);
+//    public void solution(int c){
+//        System.out.println((char)c);
+//        if(c=='z') return;
+//        solution(c+1);
+//    }
+
+    public void alphabetCombination(char c, char prefix) {
+        if (c > 'z') return;
+        System.out.println(prefix + " " + c);
+        alphabetCombination((char) (c + 1), prefix);
+    }
+
+    public void solution(char c) {
+        if (c > 'z') return;
+        alphabetCombination('a', c);
+        solution((char) (c + 1));
     }
 
     public static void main(String[] args) {

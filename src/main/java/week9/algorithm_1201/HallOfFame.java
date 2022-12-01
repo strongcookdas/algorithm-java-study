3595f66b1adb337eaa2e566fc732b74deea05b23
strongcookdas <goodjav141592@gmail.com>
2022-12-01 10:26:27 +0900

    algorithm : 프로그래머스 명예의 전당 완료

---
+++ src/main/java/week9/algorithm_1201/HallOfFame.java
@@ -0,0 +1,30 @@
+package week9.algorithm_1201;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class HallOfFame {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        int idx = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < score.length; i++) {
+            if (i < k) pq.add(score[i]);
+            else {
+                if (pq.peek() < score[i]) {
+                    pq.poll();
+                    pq.add(score[i]);
+                }
+            }
+            answer[idx++] = pq.peek();
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] ans = new HallOfFame().solution(3, new int[]{10, 100, 20, 150, 1, 100, 200});
+        System.out.println(Arrays.toString(ans));
+    }
+}

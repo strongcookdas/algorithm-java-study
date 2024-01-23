78549d0c537fb803513c514fea608a2604ca3941
strongcookdas <goodjav141592@gmail.com>
2024-01-23 23:07:01 +0900

    chore : 프로그래머스 올바른 괄호

---
+++ src/main/java/algorithm/stack/ProgrammersSolution.java
@@ -0,0 +1,21 @@
+package algorithm.stack;
+
+import java.util.*;
+
+class ProgrammersSolution {
+
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+            } else if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}

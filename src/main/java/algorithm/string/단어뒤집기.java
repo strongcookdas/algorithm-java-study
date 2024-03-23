76dd833400d3980e41c5dafe777f5d8c32c6e9d1
strongcookdas <goodjav141592@gmail.com>
2024-03-23 16:14:00 +0900

    chore : 단어 뒤집기 - [인프런] 자바 알고리즘 문제풀이 입문

---
+++ src/main/java/algorithm/string/단어뒤집기.java
@@ -0,0 +1,40 @@
+package algorithm.string;
+
+import java.util.*;
+
+public class 단어뒤집기 {
+    public static ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+//        for (String s : str) {
+//            StringBuilder tmp = new StringBuilder(s);
+//            answer.add(tmp.reverse().toString());
+//        }
+        for (String x : str) {
+            char[] s = x.toCharArray();
+            int lt = 0;
+            int rt = x.length()-1;
+            while (lt < rt) {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s);
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = scanner.next();
+        }
+        for (String s : solution(n, str)) {
+            System.out.println(s);
+        }
+    }
+}

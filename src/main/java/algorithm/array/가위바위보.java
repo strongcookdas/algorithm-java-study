c367f2d3f183fbf8973acc17bb9c326589e4dba8
strongcookdas <goodjav141592@gmail.com>
2024-03-27 13:59:01 +0900

    chore : [인프런] 가위바위보

---
+++ src/main/java/algorithm/array/가위바위보.java
@@ -0,0 +1,34 @@
+package algorithm.array;
+
+import java.util.*;
+
+public class 가위바위보 {
+    public static ArrayList<String> solution(int[] arr1, int[] arr2) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (int i = 0; i < arr1.length; i++) {
+            if(arr1[i] == arr2[i]) answer.add("D");
+            else if(arr1[i]==1 && arr2[i]==3) answer.add("A");
+            else if(arr1[i]==2 && arr2[i]==1) answer.add("A");
+            else if(arr1[i]==3 && arr2[i]==2) answer.add("A");
+            else answer.add("B");
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            arr2[i] = scanner.nextInt();
+        }
+        ArrayList<String> answer = solution(arr1,arr2);
+        for (String s : answer) {
+            System.out.println(s);
+        }
+    }
+}

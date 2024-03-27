70cf899bfa69720e7163b673b10ee8a915fb24af
strongcookdas <goodjav141592@gmail.com>
2024-03-27 13:42:37 +0900

    chore : [인프런] 보이는 학생

---
+++ src/main/java/algorithm/array/보이는학생.java
@@ -0,0 +1,27 @@
+package algorithm.array;
+
+import java.util.*;
+
+public class 보이는학생 {
+    public static int solution(int[] arr) {
+        int max = arr[0];
+        int count = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                count++;
+                max = arr[i];
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(solution(arr));
+    }
+}

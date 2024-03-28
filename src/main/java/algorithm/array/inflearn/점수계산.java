6e58d4b94a26dfa0dc58a7f7e5e08777d386e602
strongcookdas <goodjav141592@gmail.com>
2024-03-28 13:38:37 +0900

    chore : [인프런] 점수 계산

---
+++ src/main/java/algorithm/array/inflearn/점수계산.java
@@ -0,0 +1,29 @@
+package algorithm.array.inflearn;
+
+import java.util.*;
+
+public class 점수계산 {
+    public static int solution(int[] arr) {
+        int answer = 0;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                count = 0;
+                continue;
+            }
+            count++;
+            answer += count;
+        }
+        return answer;
+    }
+
+    public static void main(String[] argus) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(solution(arr));
+    }
+}

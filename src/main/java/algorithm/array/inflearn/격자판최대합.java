c105152e8e085860f4ef1df0d027423f4a82aa3a
strongcookdas <goodjav141592@gmail.com>
2024-03-28 14:33:28 +0900

    chore : [인프런] 격자판최대합

---
+++ src/main/java/algorithm/array/inflearn/격자판최대합.java
@@ -0,0 +1,39 @@
+package algorithm.array.inflearn;
+
+import java.util.*;
+
+public class 격자판최대합 {
+    public static int solution(int[][] arr) {
+        int answer = 0;
+        int sum1, sum2;
+        for (int i = 0; i < arr.length; i++) {
+            sum1 = sum2 = 0;
+            for (int j = 0; j < arr.length; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+        sum1 = sum2 = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[i][arr.length - i - 1];
+        }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println(solution(arr));
+    }
+}

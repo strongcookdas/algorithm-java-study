62f2989ad658c19bb5078e273bb22b04287fe09a
strongcookdas <goodjav141592@gmail.com>
2024-03-29 15:15:48 +0900

    chore : [인프런] 두배열합치기

---
+++ src/main/java/algorithm/two_pointer/inflearn/두배열합치기.java
@@ -0,0 +1,34 @@
+package algorithm.two_pointer.inflearn;
+
+import java.util.*;
+
+public class 두배열합치기 {
+    public static ArrayList<Integer> solution(int n, int m, int[] arr1, int[] arr2) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int p1 = 0, p2 = 0;
+        while (p1 < n && p2 < m) {
+            if (arr1[p1] < arr2[p2]) answer.add(arr1[p1++]);
+            else answer.add(arr2[p2++]);
+        }
+        while (p1 < n) answer.add(arr1[p1++]);
+        while (p2 < m) answer.add(arr2[p2++]);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = scanner.nextInt();
+        }
+        int m = scanner.nextInt();
+        int[] arr2 = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr2[i] = scanner.nextInt();
+        }
+        for (Integer i : solution(n, m, arr1, arr2)) {
+            System.out.print(i + " ");
+        }
+    }
+}

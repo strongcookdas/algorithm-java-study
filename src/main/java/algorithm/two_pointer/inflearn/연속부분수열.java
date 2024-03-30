305c432baf28a1b5c0a6a188b5ae976f8525b597
strongcookdas <goodjav141592@gmail.com>
2024-03-30 23:38:32 +0900

    chore : [인프런] 연속부분수열 *

---
+++ src/main/java/algorithm/two_pointer/inflearn/연속부분수열.java
@@ -0,0 +1,32 @@
+package algorithm.two_pointer.inflearn;
+
+import java.util.*;
+
+// *
+public class 연속부분수열 {
+    public static int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        int sum = 0;
+        int lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            sum += arr[rt];
+            if (sum == m) answer++;
+            while (sum >= m) {
+                sum -= arr[lt++];
+                if (sum == m) answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(solution(n, m, arr));
+    }
+}

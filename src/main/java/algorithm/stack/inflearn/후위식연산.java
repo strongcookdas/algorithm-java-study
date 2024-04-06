e6ae55d188937e03d8a5aaff8c0377bc26cc8891
strongcookdas <goodjav141592@gmail.com>
2024-04-06 23:50:34 +0900

    chore : [인프런] 후위식연산

---
+++ src/main/java/algorithm/stack/inflearn/후위식연산.java
@@ -0,0 +1,27 @@
+package algorithm.stack.inflearn;
+
+import java.util.*;
+
+public class 후위식연산 {
+    public static int solution(String str) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (char x : str.toCharArray()) {
+            if (Character.isDigit(x)) stack.push(x - '0');
+            else {
+                int rt = stack.pop();
+                int lt = stack.pop();
+                if (x == '+') stack.push(lt + rt);
+                else if (x == '-') stack.push(lt - rt);
+                else if (x == '*') stack.push(lt * rt);
+                else if (x == '/') stack.push(lt / rt);
+            }
+        }
+        return stack.get(0);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(solution(scanner.nextLine()));
+    }
+}

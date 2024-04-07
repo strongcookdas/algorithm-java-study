58fdd8a314de647a41ca323149ce437419d4f668
strongcookdas <goodjav141592@gmail.com>
2024-04-07 16:19:30 +0900

    chore : [인프런] 공주구하기

---
+++ src/main/java/algorithm/stack/inflearn/공주구하기.java
@@ -0,0 +1,28 @@
+package algorithm.stack.inflearn;
+
+import java.util.*;
+
+public class 공주구하기 {//다시 풀기, 문제 좀 잘 좀 읽자;;;;
+    public static int solution(int n, int k) {
+        Queue<Integer> queue = new LinkedList<>();
+        int count = 1;
+        for (int i = 1; i <= n; i++) {
+            queue.offer(i);
+        }
+        while (queue.size() != 1) {
+            if (count == k) {
+                queue.poll();
+                count = 1;
+            } else {
+                queue.offer(queue.poll());
+                count++;
+            }
+        }
+        return queue.peek();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(solution(scanner.nextInt(), scanner.nextInt()));
+    }
+}

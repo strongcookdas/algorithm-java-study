b69315207c3d0ab88daae9eaefac4976908b3c48
strongcookdas <goodjav141592@gmail.com>
2024-04-09 00:59:09 +0900

    chore : [인프런] 응급실 *

---
+++ src/main/java/algorithm/stack_queue/README.md
@@ -0,0 +1,16 @@
+# Stack Queue
+
+## Stack 함수
+- pop()
+- push(value)
+- size()
+- isEmpty()
+---
+## Queue 함수
+- poll()
+- offer(value)
+- size()
+- isEmpty()
+---
+## 다시 풀어야 하는 문제
+- [인프런] 응급실
--- src/main/java/algorithm/stack_queue/inflearn/응급실.java
+++ src/main/java/algorithm/stack_queue/inflearn/응급실.java
@@ -6,9 +6,10 @@ import java.util.*;
 - 해당문제는 큐 말고도 정렬로 풀 수 있을 것 같았지만 같은 위험도를 가졌을 때 코드가 복잡해진다.
 - 아.. 큐 1개로는 같은 위험도 구분하기 어려움...
 - 큐 2개로 구현
+- 이건 java다... 큐 두개 만들 필요없이 클래스로 구현하면 된다.. (다시 보면 클래스로 해결하도록)
  */
 public class 응급실 {
-    public static int solution(int n, int m, int[] arr) {
+    public static int solution(int n, int m, int[] arr) { // 다시
         int answer = 0;
         Queue<Integer> idxQueue = new LinkedList<>();
         Queue<Integer> riskQueue = new LinkedList<>();

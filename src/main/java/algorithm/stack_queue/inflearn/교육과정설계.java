f8b94f5e5a655faad997b92b2aafaab17013ed96
strongcookdas <goodjav141592@gmail.com>
2024-04-08 23:59:56 +0900

    chore : [인프런] 교육과정설계 - 나의 풀이

---
+++ src/main/java/algorithm/stack_queue/inflearn/교육과정설계.java
@@ -0,0 +1,42 @@
+package algorithm.stack_queue.inflearn;
+
+import java.util.*;
+
+/*
+- 큐 사용
+- 수업표 반복문 돌면서 필수과목에 포함되어 있는지 확인
+    - 있으면 순서 확인
+        순서 틀리면 return NO
+        맞다면 poll
+    - 없으면 poll
+ */
+public class 교육과정설계 {
+    public static String solution(String s, String t) {
+        String answer = "YES";
+        Queue<Character> eSubjects = new LinkedList<>();
+        Queue<Character> subjects = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            eSubjects.offer(c);
+        }
+        for (char c : t.toCharArray()) {
+            subjects.offer(c);
+        }
+        while (!subjects.isEmpty()) {
+            if (eSubjects.contains(subjects.peek())) {
+                if (eSubjects.peek() == subjects.peek()) {
+                    eSubjects.poll();
+                    subjects.poll();
+                } else return "NO";
+            } else {
+                subjects.poll();
+            }
+        }
+        if (!eSubjects.isEmpty()) return "NO";
+        return "YES";
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(solution(scanner.nextLine(), scanner.nextLine()));
+    }
+}

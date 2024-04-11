683a1a0ada1acb9d947d4df0d7587babb2c33399
strongcookdas <goodjav141592@gmail.com>
2024-04-11 18:37:52 +0900

    chore : [인프런] LRU - 일반 배열 사용

--- READMD.md
+++ READMD.md
@@ -1,2 +1,11 @@
 # 알고리즘
+
 <hr/>
+
+## 자료구조 Method 정리
+
+- ArrayList
+    - remove(idx) 특정 인덱스 원소 삭제
+    - add(idx, value) 특정 인덱스에 원소 추가
+    - indexOf(value) 첫번째 value의 인덱스 반환 없으면 -1
+    - size() 크기 반환
--- src/main/java/algorithm/sort/inflearn/LRU.java
+++ src/main/java/algorithm/sort/inflearn/LRU.java
@@ -2,18 +2,22 @@ package algorithm.sort.inflearn;
 
 import java.util.*;
 
-public class LRU { // time : 10m
-    public static ArrayList<Integer> solution(int n, int m, int[] arr) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        for (int i : arr) {
-            int idx = answer.indexOf(i);
-            if (idx != -1) {
-                answer.remove(idx);
-                answer.add(0, i);
+public class LRU { // 자료구조 사용하지 않고 일반 배열 사용, 나중에 일반 배열로 다시 풀기
+    public static int[] solution(int n, int m, int[] arr) {
+        int[] answer = new int[n];
+        for (int x : arr) {
+            int pos = -1;
+            for (int i = 0; i < n; i++) if (x == answer[i]) pos = i;
+            if (pos == -1) {
+                for (int i = n - 1; i >= 1; i--) {
+                    answer[i] = answer[i - 1];
+                }
             } else {
-                if (answer.size() == n) answer.remove(answer.size() - 1);
-                answer.add(0, i);
+                for (int i = pos; i >= 1; i--) {
+                    answer[i] = answer[i - 1];
+                }
             }
+            answer[0] = x;
         }
         return answer;
     }

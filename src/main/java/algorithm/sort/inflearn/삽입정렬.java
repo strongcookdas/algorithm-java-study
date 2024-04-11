976821369f8875082458856137e05b9368348a22
strongcookdas <goodjav141592@gmail.com>
2024-04-11 16:28:46 +0900

    chore : [인프런] 삽입정렬

---
+++ src/main/java/algorithm/sort/inflearn/삽입정렬.java
@@ -0,0 +1,26 @@
+package algorithm.sort.inflearn;
+
+import java.util.Scanner;
+
+public class 삽입정렬 {
+    public static int[] solution(int n, int[] arr) {
+        for (int i = 1; i < n; i++) {
+            int tmp = arr[i];
+            int j;
+            for (j = i - 1; j >= 0; j--) {
+                if (arr[j] > tmp) arr[j + 1] = arr[j];
+                else break;
+            }
+            arr[j+1] = tmp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] argus) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = scanner.nextInt();
+        for (int i : solution(n, arr)) System.out.print(i + " ");
+    }
+}

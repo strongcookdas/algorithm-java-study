a26d30a0333085c6566d507e61b3293dad2848bc
strongcookdas <goodjav141592@gmail.com>
2024-04-11 15:59:24 +0900

    chore : [인프런] 버블정렬

---
+++ src/main/java/algorithm/sort/inflearn/버블정렬.java
@@ -0,0 +1,30 @@
+package algorithm.sort.inflearn;
+
+import java.util.*;
+
+public class 버블정렬 {
+    public static int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] argus) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        for (int i : solution(n, arr)) {
+            System.out.print(i + " ");
+        }
+    }
+}

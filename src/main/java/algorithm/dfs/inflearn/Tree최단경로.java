72fd4acf764b2d327352a731f2ca1dfd54995747
strongcookdas <goodjav141592@gmail.com>
2024-04-12 23:04:37 +0900

    chore : [인프런] Tree 최단경로 DFS

---
+++ src/main/java/algorithm/dfs/inflearn/Tree최단경로.java
@@ -0,0 +1,22 @@
+package algorithm.dfs.inflearn;
+
+public class Tree최단경로 {
+    static Node root;
+
+    public static int DFS(int L, Node root) {
+        if (root.lt == null && root.rt == null) return L;
+        else return Math.min(DFS(L + 1, root.lt), DFS(L + 1, root.rt));
+    }
+
+    public static void main(String[] args) {
+        root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+        root.rt.lt = new Node(6);
+        root.rt.rt = new Node(7);
+        System.out.println(DFS(0,root));
+    }
+
+}

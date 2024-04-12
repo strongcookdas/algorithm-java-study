8633c1b0b2d3686b297480456467e6cb0d8fe9a0
strongcookdas <goodjav141592@gmail.com>
2024-04-12 18:58:15 +0900

    chore : [인프런] 이진트리

---
+++ src/main/java/algorithm/recursion/inflearn/이진트리순회.java
@@ -0,0 +1,35 @@
+package algorithm.recursion.inflearn;
+
+class Node {
+    int data;
+    Node lt, rt;
+
+    public Node(int value) {
+        this.data = value;
+        lt = rt = null;
+    }
+}
+
+public class 이진트리순회 {
+    static Node root;
+
+    public static void DFS(Node root) {
+        if (root == null) return;
+//        System.out.print(root.data+" "); //전위 순회
+        DFS(root.lt);
+        System.out.print(root.data+" "); //중위 순회
+        DFS(root.rt);
+//        System.out.print(root.data+" "); //후위 순회
+    }
+
+    public static void main(String[] args) {
+        root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+        root.rt.lt = new Node(6);
+        root.rt.rt = new Node(7);
+        DFS(root);
+    }
+}

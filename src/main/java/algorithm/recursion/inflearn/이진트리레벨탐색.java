b3b3f800e550911509b93289c9c794d1602ceff2
strongcookdas <goodjav141592@gmail.com>
2024-04-12 20:45:28 +0900

    chore : [인프런] 이진트리레벨탐색

---
+++ src/main/java/algorithm/recursion/inflearn/이진트리레벨탐색.java
@@ -0,0 +1,36 @@
+package algorithm.recursion.inflearn;
+
+import java.util.*;
+
+public class 이진트리레벨탐색 {
+    static Node root;
+
+    public static void BFS(Node root) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(root);
+        int L = 0;
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+            System.out.print(L + " : ");
+            for (int i = 0; i < len; i++) {
+                Node cur = Q.poll();
+                System.out.print(cur.data + " ");
+                if (cur.lt != null) Q.offer(cur.lt);
+                if (cur.lt != null) Q.offer(cur.rt);
+            }
+            L++;
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+        root.rt.lt = new Node(6);
+        root.rt.rt = new Node(7);
+        BFS(root);
+    }
+}

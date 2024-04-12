efeaa9a8b9ce5da3a8915ce65d671cd9e25df301
strongcookdas <goodjav141592@gmail.com>
2024-04-12 20:25:43 +0900

    chore : [인프런]

---
+++ src/main/java/algorithm/recursion/inflearn/부분집합.java
@@ -0,0 +1,29 @@
+package algorithm.recursion.inflearn;
+
+import java.util.*;
+
+public class 부분집합 {
+    static int n;
+    static int[] ch;
+
+    public static void DFS(int L) {
+        if (L == n + 1) {
+            for (int i = 1; i <= n; i++) {
+                if (ch[i] == 1) System.out.print(i + " ");
+            }
+            System.out.println();
+        } else {
+            ch[L] = 1;
+            DFS(L + 1);
+            ch[L] = 0;
+            DFS(L + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        n = 3;
+        ch = new int[n + 1];
+        DFS(1);
+    }
+
+}

fc7566d1b05d1a1cc50be4b5e77d7f7df32ea517
strongcookdas <goodjav141592@gmail.com>
2024-04-18 20:00:29 +0900

    chore : [인프런] 미로의 최단거리 통로 - time : 15m - 시도 : 2 - DFS로 풀었다.

---
+++ src/main/java/algorithm/dfs/inflearn/미로의최단거리통로DFS.java
@@ -0,0 +1,38 @@
+package algorithm.dfs.inflearn;
+
+import java.util.Scanner;
+
+public class 미로의최단거리통로DFS {
+    static int answer = Integer.MAX_VALUE;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+    static int[][] board = new int[7][7];
+
+    public static void DFS(int x, int y, int dist) {
+        if (x == 6 && y == 6) {
+            answer = Math.min(answer, dist);
+            return;
+        }
+        if (dist > answer) return;
+        for (int i = 0; i < dx.length; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < 7 && ny >= 0 && ny < 7 && board[ny][nx] == 0) {
+                board[ny][nx] = 1;
+                DFS(nx, ny, dist + 1);
+                board[ny][nx] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) board[i][j] = kb.nextInt();
+        }
+        board[0][0] = 1;
+        DFS(0, 0, 0);
+        System.out.println((answer == Integer.MAX_VALUE) ? -1 : answer);
+    }
+
+}

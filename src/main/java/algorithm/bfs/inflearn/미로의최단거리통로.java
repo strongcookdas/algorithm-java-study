94ff4d9c37d7421b508145364913c50a62934cf0
strongcookdas <goodjav141592@gmail.com>
2024-04-18 20:29:25 +0900

    chore : [인프런] 미로의 최단거리 통로 - 모범답안

--- src/main/java/algorithm/bfs/inflearn/미로의최단거리통로.java
+++ src/main/java/algorithm/bfs/inflearn/미로의최단거리통로.java
@@ -2,37 +2,49 @@ package algorithm.bfs.inflearn;
 
 import java.util.*;
 
+class Point {
+    public int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class 미로의최단거리통로 {
     static int answer = Integer.MAX_VALUE;
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {-1, 1, 0, 0};
-    static int[][] board = new int[7][7];
+    static int[][] board, dis;
 
-    public static void DFS(int x, int y, int dist) {
-        if (x == 6 && y == 6) {
-            answer = Math.min(answer, dist);
-            return;
-        }
-        if (dist > answer) return;
-        for (int i = 0; i < dx.length; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && nx < 7 && ny >= 0 && ny < 7 && board[ny][nx] == 0) {
-                board[ny][nx] = 1;
-                DFS(nx, ny, dist + 1);
-                board[ny][nx] = 0;
+    public static void BFS(int x, int y) {
+        Queue<Point> Q = new LinkedList<>();
+        Q.offer(new Point(x, y));
+        board[x][y] = 1;
+        while (!Q.isEmpty()) {
+            Point tmp = Q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = tmp.x + dx[i];
+                int ny = tmp.y + dy[i];
+                if (nx >= 1 && nx <= 7 && ny >= 1 && ny <= 7 && board[nx][ny] == 0) {
+                    board[nx][ny] = 1;
+                    Q.offer(new Point(nx, ny));
+                    dis[nx][ny] = dis[tmp.x][tmp.y] + 1;
+                }
             }
         }
     }
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 7; j++) board[i][j] = kb.nextInt();
+        board = new int[8][8];
+        dis = new int[8][8];
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 7; j++) board[i][j] = kb.nextInt();
         }
-        board[0][0] = 1;
-        DFS(0, 0, 0);
-        System.out.println((answer == Integer.MAX_VALUE) ? -1 : answer);
+        BFS(1, 1);
+        if (dis[7][7] == 0) System.out.println(-1);
+        else System.out.println(dis[7][7]);
     }
 
 }

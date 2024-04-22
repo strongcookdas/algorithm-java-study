2bd518dafae93137a3c8b2ba6d127b18b8bb727d
strongcookdas <goodjav141592@gmail.com>
2024-04-22 12:32:36 +0900

    chore : [인프런] 씨름선수 모범답안

--- src/main/java/algorithm/greedy/inflearn/씨름선수.java
+++ src/main/java/algorithm/greedy/inflearn/씨름선수.java
@@ -1,4 +1,10 @@
 package algorithm.greedy.inflearn;
+/*
+1. 이중 for문으로 풀면 시간 초과
+2. 키순으로 정렬 후 내 앞사람보다 몸무게가 크면 그 사람 카운트
+3. 몸무게가 앞사람보다 낮으면 키와 몸무게가 자신보다 큰 사람이 있다는 의미
+4. 1개의 for문으로 풀어야 한다. (전 방법으로 푼 것은 원래 시간초과가 났어야 했다.)
+ */
 
 import java.util.*;
 
@@ -19,16 +25,19 @@ class Player implements Comparable<Player> {
 }
 
 public class 씨름선수 {
-    static int n, answer;
+    static int n, answer = 1;
     static ArrayList<Player> players = new ArrayList<>();
 
     public static void solution() {
-        for (Player p1 : players) {
-            for (Player p2 : players) {
-                if (p1.height < p2.height && p1.weight < p2.weight) {
-                    answer--;
-                    break;
-                }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                max = players.get(i).weight;
+                continue;
+            }
+            if (players.get(i).weight > max) {
+                max = players.get(i).weight;
+                answer++;
             }
         }
     }
@@ -36,7 +45,6 @@ public class 씨름선수 {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         n = kb.nextInt();
-        answer = n;
         for (int i = 0; i < n; i++) players.add(new Player(kb.nextInt(), kb.nextInt()));
         Collections.sort(players);
         solution();

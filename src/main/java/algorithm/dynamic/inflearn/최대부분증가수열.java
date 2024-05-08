bc6488e85adde2974124c2aed0f83a01ee48bc1a
strongcookdas <goodjav141592@gmail.com>
2024-05-08 17:45:25 +0900

    chore : [인프런] 최대부분증가수열

--- src/main/java/algorithm/dynamic/inflearn/최대부분증가수열.java
+++ src/main/java/algorithm/dynamic/inflearn/최대부분증가수열.java
@@ -1,4 +1,30 @@
 package algorithm.dynamic.inflearn;
 
+import java.util.*;
+
 public class 최대부분증가수열 {
+    static int[] dy;
+
+    public static int solution(int[] arr) {
+        int answer = 0;
+        dy[0] = 1;
+        for (int i = 1; i < arr.length; i++) {
+            int max = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] < arr[i] && dy[j] > max) max = dy[j];
+            }
+            dy[i] = max + 1;
+            answer = Math.max(answer, dy[i]);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        dy = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+        System.out.println(solution(arr));
+    }
 }

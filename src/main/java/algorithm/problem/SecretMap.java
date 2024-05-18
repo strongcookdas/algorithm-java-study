4a542c26730467f2e1060e462294b6ffc6fc09af
strongcookdas <goodjav141592@gmail.com>
2024-05-18 22:24:55 +0900

    chore : [swea] 회문1 | 20m | 제출 1

--- src/main/java/algorithm/problem/SecretMap.java
+++ src/main/java/algorithm/problem/SecretMap.java
@@ -10,22 +10,22 @@ import java.util.Arrays;
  */
 public class SecretMap {// 프로그래머스 비밀지도
 
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-
-        String[] ans = new String[n];
-        for (int i = 0; i < n; i++) {
-            String temp = Integer.toBinaryString(arr1[i] | arr2[i]);
-            String str = "0".repeat(n - temp.length());
-            str += temp;
-            str = str.replace("0", " ");
-            str = str.replace("1", "#");
-            ans[i] = str;
-        }
-        return ans;
-    }
-
-    public static void main(String[] args) {
-        SecretMap secretMap = new SecretMap();
-        System.out.println(Arrays.toString(secretMap.solution(6, new int[]{46, 33, 33, 22, 31, 50}, new int[]{27, 56, 19, 14, 14, 10})));
-    }
+//    public String[] solution(int n, int[] arr1, int[] arr2) {
+//
+//        String[] ans = new String[n];
+//        for (int i = 0; i < n; i++) {
+//            String temp = Integer.toBinaryString(arr1[i] | arr2[i]);
+//            String str = "0".repeat(n - temp.length());
+//            str += temp;
+//            str = str.replace("0", " ");
+//            str = str.replace("1", "#");
+//            ans[i] = str;
+//        }
+//        return ans;
+//    }
+//
+//    public static void main(String[] args) {
+//        SecretMap secretMap = new SecretMap();
+//        System.out.println(Arrays.toString(secretMap.solution(6, new int[]{46, 33, 33, 22, 31, 50}, new int[]{27, 56, 19, 14, 14, 10})));
+//    }
 }

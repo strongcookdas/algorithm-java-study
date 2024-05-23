bfc1391e6978f121a259ea65f434f2c105f389bd
strongcookdas <goodjav141592@gmail.com>
2024-05-23 22:18:51 +0900

    chore : [인프런] DFS/BFS 복습 | 피보나치

--- src/main/java/algorithm/recursion/inflearn/피보나치.java
+++ src/main/java/algorithm/recursion/inflearn/피보나치.java
@@ -1,19 +1,21 @@
 package algorithm.recursion.inflearn;
 
-public class 피보나치 {
-    //시간 효율성을 위해서는 재귀로만 풀면 x (메모이제이션 활용)
-    static int[] fibo;
-    public static int DFS(int n) {
-        if (n == 1) return fibo[n] = 1;
-        else if (n == 2) return fibo[n] = 1;
-        else if (fibo[n]!=0) return fibo[n];
-        else return fibo[n] = DFS(n - 2) + DFS(n - 1);
-    }
+import java.util.*;
 
-    public static void main(String[] args) {
-        int n = 45;
-        fibo = new int[n+1];
-        DFS(n);
-        for (int i = 1; i <= n; i++) System.out.println(fibo[i]);
+public class 피보나치 {
+    static int[] fibo;
+    public static int solution(int n){
+        if(n==0||n==1) return fibo[n] = 1;
+        if(fibo[n]!=0) return fibo[n];
+        return fibo[n] = solution(n-2) + solution(n-1);
+    }
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        fibo = new int[n];
+        solution(n-1);
+        for(int v : fibo){
+            System.out.print(v+" ");
+        }
     }
 }

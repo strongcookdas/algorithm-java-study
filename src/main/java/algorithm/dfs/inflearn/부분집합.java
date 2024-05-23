c2051c4f0f910fc0678a26ad0acb24957df074b9
strongcookdas <goodjav141592@gmail.com>
2024-05-23 23:14:13 +0900

    chore : [인프런] DFS/BFS 복습 | 부분집합

--- src/main/java/algorithm/dfs/inflearn/부분집합.java
+++ src/main/java/algorithm/dfs/inflearn/부분집합.java
@@ -2,28 +2,36 @@ package algorithm.dfs.inflearn;
 
 import java.util.*;
 
+/*
+부분 집합
+- 원소가 있거나 없거나 둘중 하나
+    - 원소가 있을 경우 깊이 우선 탐색
+    - 없을 경우 깊이 우선 탐색
+    - 출력 레벨이 n+1이면
+ */
+
 public class 부분집합 {
+    static boolean[] visit;
     static int n;
-    static int[] ch;
 
     public static void DFS(int L) {
         if (L == n + 1) {
-            for (int i = 1; i <= n; i++) {
-                if (ch[i] == 1) System.out.print(i + " ");
-            }
-            System.out.println();
-        } else {
-            ch[L] = 1;
-            DFS(L + 1);
-            ch[L] = 0;
-            DFS(L + 1);
+            String tmp = "";
+            for (int i = 1; i <= n; i++) if (visit[i]) tmp += (i + " ");
+            if (tmp.length() > 0) System.out.println(tmp);
+            return;
         }
+        visit[L] = true;
+        DFS(L + 1);
+        visit[L] = false;
+        DFS(L + 1);
     }
 
     public static void main(String[] args) {
-        n = 3;
-        ch = new int[n + 1];
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        visit = new boolean[n + 1];
+        Arrays.fill(visit, true);
         DFS(1);
     }
-
 }

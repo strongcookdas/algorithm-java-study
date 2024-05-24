ef2f5b5d6f395279ddcb2e5cbbd883957231cb47
strongcookdas <goodjav141592@gmail.com>
2024-05-24 13:32:33 +0900

    chore : [인프런 복습] bfs/dfs | 경로탐색 (dfs 활용)

--- src/main/java/algorithm/dfs/inflearn/경로탐색.java
+++ src/main/java/algorithm/dfs/inflearn/경로탐색.java
@@ -3,32 +3,33 @@ package algorithm.dfs.inflearn;
 import java.util.*;
 
 public class 경로탐색 {
-    static int n, m, answer = 0;
     static int[][] graph;
     static int[] ch;
-    public static void DFS(int v){
-        if(v==n) answer++;
-        else{
-            for(int i = 1; i<=n; i++){
-                if(graph[v][i]==1 && ch[i] == 0){
-                    ch[i]=1;
-                    DFS(i);
-                    ch[i]=0;
-                }
+    static int N,M, answer = 0;
+
+    public static void DFS(int node){
+        if(node == N) {
+            answer++;
+            return;
+        }
+        for(int i = 1; i<=N; i++){
+            if(graph[node][i]==1&&ch[i]==0){
+                ch[i] = 1;
+                DFS(i);
+                ch[i] = 0;
             }
         }
     }
-
     public static void main(String[] args){
         Scanner kb = new Scanner(System.in);
-        n = kb.nextInt();
-        m = kb.nextInt();
-        graph = new int[n+1][n+1];
-        ch = new int[n+1];
-        for(int i = 0; i<m; i++){
-            int a = kb.nextInt();
-            int b = kb.nextInt();
-            graph[a][b] = 1;
+        N = kb.nextInt();
+        M = kb.nextInt();
+        graph = new int[N+1][N+1];
+        ch = new int[N+1];
+        for(int i = 0; i<M; i++){
+            int n1 = kb.nextInt();
+            int n2 = kb.nextInt();
+            graph[n1][n2] = 1;
         }
         ch[1] = 1;
         DFS(1);

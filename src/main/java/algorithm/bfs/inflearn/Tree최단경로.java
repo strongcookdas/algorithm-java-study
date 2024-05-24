9f96abc1ba19a076c4afcfd1b36ed7683e3772f4
strongcookdas <goodjav141592@gmail.com>
2024-05-24 13:09:36 +0900

    chore : [인프런 복습] bfs/dfs | 말단노드까지의 최단거리

--- src/main/java/algorithm/bfs/inflearn/Tree최단경로.java
+++ src/main/java/algorithm/bfs/inflearn/Tree최단경로.java
@@ -1,34 +1,33 @@
 package algorithm.bfs.inflearn;
 
 import java.util.*;
+
 public class Tree최단경로 {
-    static Node root;
+    static Queue<Node> Q;
 
     public static int BFS(Node root) {
-        Queue<Node> Q = new LinkedList<>();
+        int answer = 0;
+        Q = new LinkedList<>();
         Q.offer(root);
-        int L = 0;
-        while(!Q.isEmpty()){
-            int len = Q.size();
-            for(int i = 0; i<len; i++){
-                Node cur = Q.poll();
-                if(cur.lt==null&&cur.rt==null) return L;
-                if(cur.lt!=null) Q.offer(cur.lt);
-                if(cur.rt!=null) Q.offer(cur.rt);
+        while (!Q.isEmpty()) {
+            int n = Q.size();
+            for (int i = 0; i < n; i++) {
+                Node tmp = Q.poll();
+                if (tmp.lt == null && tmp.rt == null) return answer;
+                if (tmp.lt != null) Q.offer(tmp.lt);
+                if (tmp.rt != null) Q.offer(tmp.rt);
             }
-            L++;
+            answer++;
         }
-        return L;
+        return 0;
     }
 
     public static void main(String[] args) {
-        root = new Node(1);
+        Node root = new Node(1);
         root.lt = new Node(2);
         root.rt = new Node(3);
         root.lt.lt = new Node(4);
         root.lt.rt = new Node(5);
-        root.rt.lt = new Node(6);
-        root.rt.rt = new Node(7);
         System.out.println(BFS(root));
     }
 }

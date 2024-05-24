a2ab32c3e3c90850c5536e4549bfbcd62c391753
strongcookdas <goodjav141592@gmail.com>
2024-05-24 16:39:58 +0900

    chore : [인프런 복습] bfs/dfs | 순열구하기 | 10m | 제출 1

--- src/main/java/algorithm/dfs/inflearn/순열구하기.java
+++ src/main/java/algorithm/dfs/inflearn/순열구하기.java
@@ -1,35 +1,43 @@
 package algorithm.dfs.inflearn;
 
+/*
+- DFS 활용
+    - 중복 순열 구하는 방식 활용
+    - 단, visit 배열 추가하여 해당 원소 추가했는지 확인하는 로직 추가
+- 종료 조건
+    - L == M
+ */
+
 import java.util.*;
 
 public class 순열구하기 {
-    static int n;
-    static int m;
-    static int[] pm, ch, arr;
+    static int N, M;
+    static int[] arr, visit, pm;
 
     public static void DFS(int L) {
-        if (L == m) {
-            for (int i : pm) System.out.print(i + " ");
+        if (L == M) {
+            for (int n : pm) System.out.print(n + " ");
             System.out.println();
-            return;
-        }
-        for (int i = 0; i < n; i++) {
-            if (ch[i] != 0) continue;
-            pm[L] = arr[i];
-            ch[i] = 1;
-            DFS(L + 1);
-            ch[i] = 0;
+        }else{
+            for(int i = 0; i<N; i++){
+                if(visit[i]==0){
+                    visit[i] = 1;
+                    pm[L] = arr[i];
+                    DFS(L+1);
+                    visit[i] = 0;
+                }
+            }
         }
     }
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        n = kb.nextInt();
-        m = kb.nextInt();
-        arr = new int[n];
-        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
-        ch = new int[n];
-        pm = new int[m];
+        N = kb.nextInt();
+        M = kb.nextInt();
+        arr = new int[N];
+        visit = new int[N];
+        pm = new int[M];
+        for (int i = 0; i < N; i++) arr[i] = kb.nextInt();
         DFS(0);
     }
 }

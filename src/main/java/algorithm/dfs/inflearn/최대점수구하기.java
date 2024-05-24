1f81ca94e620aa74d309566602bcb665388cd092
strongcookdas <goodjav141592@gmail.com>
2024-05-24 16:03:32 +0900

    chore : [인프런 복습] bfs/dfs | 최대점수구하기 | 5m

--- src/main/java/algorithm/dfs/inflearn/최대점수구하기.java
+++ src/main/java/algorithm/dfs/inflearn/최대점수구하기.java
@@ -1,19 +1,10 @@
 package algorithm.dfs.inflearn;
 
 /*
-- DFS 적용
-- 문제 클래스 생성
-- DFS
-    - 매개 변수
-        - 문제 배열
-        - 점수의 합
-        - 시간의 합
-        - Level
-- 모든 문제의 조합을 탐색
-    - 정해진 시간이 초과시 return
-    - 점수의 Max값을 구함
-- 걸린 시간
-    - 15분
+- DFS 활용
+- 종료 조건
+    - L == N
+    - 제한 시간이 넘어갈 때
  */
 
 import java.util.*;
@@ -29,26 +20,23 @@ class Problem {
 }
 
 public class 최대점수구하기 {
+    static int N,M, answer = Integer.MIN_VALUE;
     static Problem[] arr;
-    static int n;
-    static int timeLimit;
-    static int max = Integer.MIN_VALUE;
-
-    public static void DFS(int L, int sum, int time, Problem[] arr) {
-        if (time > timeLimit) return;
-        if (L == n) max = Math.max(max, sum);
-        else {
-            DFS(L + 1, sum + arr[L].score, time + arr[L].time, arr);
-            DFS(L + 1, sum, time, arr);
+    public static void DFS(int L, int sum, int time){
+        if(time > M) return;
+        if(L==N) answer = Math.max(sum, answer);
+        else{
+            DFS(L+1, sum+arr[L].score, time+arr[L].time);
+            DFS(L+1, sum, time);
         }
     }
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        n = kb.nextInt();
-        arr = new Problem[n];
-        timeLimit = kb.nextInt();
-        for (int i = 0; i < n; i++) arr[i] = new Problem(kb.nextInt(), kb.nextInt());
-        DFS(0,0,0,arr);
-        System.out.println(max);
+        N = kb.nextInt();
+        M = kb.nextInt();
+        arr = new Problem[N];
+        for(int i = 0; i<N; i++) arr[i] = new Problem(kb.nextInt(), kb.nextInt());
+        DFS(0,0,0);
+        System.out.println(answer);
     }
 }

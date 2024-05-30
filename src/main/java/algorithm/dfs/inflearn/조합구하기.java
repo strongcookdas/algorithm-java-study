294a3b619625f8c687c9f51442de1af9e59592e1
strongcookdas <goodjav141592@gmail.com>
2024-05-30 22:19:35 +0900

    chore : [인프런 복습] bfs/dfs | 조합 구하기 | 30m

--- src/main/java/algorithm/dfs/inflearn/조합구하기.java
+++ src/main/java/algorithm/dfs/inflearn/조합구하기.java
@@ -1,28 +1,32 @@
 package algorithm.dfs.inflearn;
+/*
+- 순열과 달리 n에 대하여 깊이 탐색 후 완료 시 ch 배열에 체크를 해제 하지 않는다.
+ */
 
 import java.util.*;
 
 public class 조합구하기 {
+    static int N, M;
     static int[] combi;
-    static int n, m;
 
-    public static void DFS(int L, int s) {
-        if (L == m) {
-            for (int x : combi) System.out.print(x + " ");
+    public static void DFS(int L, int n) {
+        if (L == M) {
+            for(int t : combi) System.out.print(t+" ");
             System.out.println();
-        } else {
-            for (int i = s; i <= n; i++) {
-                combi[L] = i;
-                DFS(L + 1, i + 1);
-            }
+            return;
         }
+        for(int i = n; i<=N; i++){
+            combi[L] = i;
+            DFS(L+1, i+1);
+        }
+
     }
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        n = kb.nextInt();
-        m = kb.nextInt();
-        combi = new int[m];
+        N = kb.nextInt();
+        M = kb.nextInt();
+        combi = new int[M];
         DFS(0, 1);
     }
 }

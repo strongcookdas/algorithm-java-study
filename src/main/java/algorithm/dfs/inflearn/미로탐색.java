869801bfe7de34953b87c08586b4f339939200fa
strongcookdas <goodjav141592@gmail.com>
2024-05-30 22:20:23 +0900

    chore : [인프런 복습] bfs/dfs | 조합 구하기 | 30m

--- src/main/java/algorithm/dfs/inflearn/미로탐색.java
+++ src/main/java/algorithm/dfs/inflearn/미로탐색.java
@@ -3,36 +3,5 @@ package algorithm.dfs.inflearn;
 import java.util.*;
 
 public class 미로탐색 {
-    static int answer = 0;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
-    static int[][] arr = new int[7][7];
 
-    public static void DFS(int x, int y) {
-        if (x == 6 && y == 6) {
-            answer++;
-            return;
-        }
-        for (int i = 0; i < dx.length; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && nx < 7 && ny >= 0 && ny < 7 && arr[ny][nx] == 0) {
-                arr[ny][nx] = 1;
-                DFS(nx,ny);
-                arr[ny][nx] = 0;
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 7; j++) {
-                arr[i][j] = kb.nextInt();
-            }
-        }
-        arr[0][0] = 1;
-        DFS(0, 0);
-        System.out.println(answer);
-    }
 }
--- src/main/java/algorithm/dfs/inflearn/조합구하기.java
+++ src/main/java/algorithm/dfs/inflearn/조합구하기.java
@@ -1,7 +1,4 @@
 package algorithm.dfs.inflearn;
-/*
-- 순열과 달리 n에 대하여 깊이 탐색 후 완료 시 ch 배열에 체크를 해제 하지 않는다.
- */
 
 import java.util.*;
 

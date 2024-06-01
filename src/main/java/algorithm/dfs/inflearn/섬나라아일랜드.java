123b16ef69f46db558aa24b5ba8749525a67a5d5
strongcookdas <goodjav141592@gmail.com>
2024-06-01 22:28:02 +0900

    chore : [인프런 복습] bfs/dfs | 섬나라 아일랜드 bfs | 20m

--- src/main/java/algorithm/dfs/inflearn/섬나라아일랜드.java
+++ src/main/java/algorithm/dfs/inflearn/섬나라아일랜드.java
@@ -3,7 +3,7 @@ package algorithm.dfs.inflearn;
 1. arr를 순회
 2. 1인 영역 DFS 탐색 (상,하,좌,우,대각선)
 3. 방문한 곳은 0으로 체크
-4. DFS 탐색이 끝나면 개수 +1
+4. BFS 탐색이 끝나면 개수 +1
 5. 2번 반복
 
 수정 후
@@ -14,6 +14,8 @@ package algorithm.dfs.inflearn;
 5. 2번 반복
  */
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 class Point {
@@ -26,23 +28,31 @@ class Point {
 }
 
 public class 섬나라아일랜드 {
-    static int N, answer=0;
-    static int[] dx = {0, 1, 1, 1, 0, -1, -1, -1, 0}, dy = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static int N, answer = 0;
+    static int[] dx = {0, 1, 1, 1, 0, -1, -1, -1,}, dy = {-1, -1, 0, 1, 1, 1, 0, -1};
     static int[][] map;
+    static Queue<Point> Q;
 
-    public static void DFS(int x, int y) {
-        if(x>=0 && x<N && y>=0 && y<N && map[y][x]==1){
-            map[y][x] = 0;
-            for (int i = 0; i < 8; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                DFS(nx,ny);
+    public static void BFS() {
+        while (!Q.isEmpty()) {
+            int n = Q.size();
+            for (int i = 0; i < n; i++) {
+                Point tmp = Q.poll();
+                for (int j = 0; j < dx.length; j++) {
+                    int nx = tmp.x + dx[j];
+                    int ny = tmp.y + dy[j];
+                    if (nx >= 0 && nx < N && ny >= 0 && ny < N && map[ny][nx] == 1) {
+                        map[ny][nx] = 0;
+                        Q.offer(new Point(nx, ny));
+                    }
+                }
             }
         }
     }
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
+        Q = new LinkedList<>();
         N = kb.nextInt();
         map = new int[N][N];
         for (int i = 0; i < N; i++) {
@@ -50,14 +60,18 @@ public class 섬나라아일랜드 {
                 map[i][j] = kb.nextInt();
             }
         }
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if(map[i][j] == 1) {
+                if (map[i][j] == 1) {
                     answer++;
-                    DFS(j, i);
+                    map[i][j] = 0;
+                    Q.offer(new Point(j, i));
+                    BFS();
                 }
             }
         }
+        BFS();
         System.out.println(answer);
     }
 }

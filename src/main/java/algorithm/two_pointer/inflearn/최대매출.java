3508f2118bb0b388758f1f9f0038f5cac85611ab
strongcookdas <goodjav141592@gmail.com>
2024-06-02 14:46:59 +0900

    chore : [인프런 복습] 투 포인터 | 최대 매출 | 15m

--- src/main/java/algorithm/two_pointer/inflearn/최대매출.java
+++ src/main/java/algorithm/two_pointer/inflearn/최대매출.java
@@ -1,48 +1,36 @@
 package algorithm.two_pointer.inflearn;
 
-import java.util.*;
 
 /*
-처음 풀이법 결국 n^2
-슬라이딩 윈도우 사용
+- 반복문으로 매출액 합을 구하면 시간 초과
+- 슬라이딩 윈도우 사용
+- O(n)으로 구할 수 있다.
  */
+
+import java.util.*;
+
 public class 최대매출 {
-    public static int solution(int n, int k, int[] arr) {
-        int answer = 0;
-//        int p1 = 0, p2 = n - 1;
-//        while (p1 < p2) {
-//            int sum1 = 0, sum2 = 0;
-//            int count = 0;
-//            while (count < k) {
-//                sum1 += arr[p1 + count++];
-//            }
-//            count = 0;
-//            while (count < k) {
-//                sum2 += arr[p2 - count++];
-//            }
-//            answer = Math.max(sum1, answer);
-//            answer = Math.max(sum2, answer);
-//            p1++;
-//            p2--;
-//        }
+    public int solution(int N, int K, int[] arr) {
+        int max = Integer.MIN_VALUE;
         int sum = 0;
-        for (int i = 0; i < k; i++) sum += arr[i];
-        answer = sum;
-        for (int i = k; i < n; i++) {
-            sum += (arr[i] - arr[i - k]);
-            answer = Math.max(answer, sum);
+        for (int i = 0; i < N - 1; i++) {
+            if (i < K) sum += arr[i];
+            else {
+                sum -= arr[i - K];
+                sum += arr[i];
+            }
+            max = Math.max(sum, max);
         }
-        return answer;
+        return max;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        int k = scanner.nextInt();
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
-        System.out.println(solution(n, k, arr));
+        최대매출 main = new 최대매출();
+        Scanner kb = new Scanner(System.in);
+        int N = kb.nextInt();
+        int K = kb.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) arr[i] = kb.nextInt();
+        System.out.println(main.solution(N, K, arr));
     }
 }

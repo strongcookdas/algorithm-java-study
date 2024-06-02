9076d74f716f425d73139569cbb5c2af7bb891d6
strongcookdas <goodjav141592@gmail.com>
2024-06-02 16:22:49 +0900

    chore : [인프런 복습] | 투 포인터 | 최대길이연속수열부분 | 20m

--- src/main/java/algorithm/two_pointer/inflearn/최대길이연속수열부분.java
+++ src/main/java/algorithm/two_pointer/inflearn/최대길이연속수열부분.java
@@ -1,38 +1,39 @@
 package algorithm.two_pointer.inflearn;
 
-import java.util.*;
 
-public class 최대길이연속수열부분 {//*
-    public static int solution(int n, int k, int[] arr) {
-        int answer = 0;
-        ArrayList<Integer> tmp = new ArrayList<>();
-        int lt = 0;
-        int count = 0;
-        for (int rt = 0; rt < n; rt++) {
-            if (arr[rt] == 1) count++;
-            else {
-                if (k > 0) {
-                    count++;
-                    k--;
-                } else {
-                    answer = Math.max(answer, count);
-                    count = (rt) - tmp.get(lt++);
+/*
+- 과정
+    - 1일 경우 슬라이딩 윈도우 증가
+    - 0일 경우
+        - k > 0 슬라이딩 윈도우 증가
+        - k < 0 첫번째 0 위치 다음으로 lt 조정 후 슬라이딩 윈도우 증가
+ */
+
+import java.util.Scanner;
+
+public class 최대길이연속수열부분 {
+    public int solution(int N, int K, int[] arr) {
+        int lt = 0, answer = Integer.MIN_VALUE;
+        for (int rt = 0; rt < N; rt++) {
+            if (arr[rt] == 0) {
+                if (K > 0) K--;
+                else {
+                    while (arr[lt] != 0) lt++;
+                    lt++;
                 }
-                tmp.add(rt);
             }
+            answer = Math.max(answer, rt - lt + 1);
         }
-        answer = Math.max(answer, count);
         return answer;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
-        System.out.println(solution(n, k, arr));
+        최대길이연속수열부분 main = new 최대길이연속수열부분();
+        Scanner kb = new Scanner(System.in);
+        int N = kb.nextInt();
+        int K = kb.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) arr[i] = kb.nextInt();
+        System.out.println(main.solution(N, K, arr));
     }
 }

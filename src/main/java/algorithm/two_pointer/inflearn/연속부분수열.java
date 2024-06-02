3e1b9d8ce5595d546695b5a8bbea8e36d895214d
strongcookdas <goodjav141592@gmail.com>
2024-06-02 15:43:48 +0900

    chore : [인프런 복습] 투 포인터 | 연속부분수열 | 1h

--- src/main/java/algorithm/two_pointer/inflearn/연속부분수열.java
+++ src/main/java/algorithm/two_pointer/inflearn/연속부분수열.java
@@ -1,32 +1,32 @@
 package algorithm.two_pointer.inflearn;
 
+/*
+- 투 포인터, 슬라이딩 윈도우 사용
+- 과정
+    - 타겟 숫자보다 작을 경우 윈도우를 1칸 늘린다.
+    - 타겟 숫자보다 클 경우 윈도우를 1칸 줄인다.
+ */
 import java.util.*;
-
-// *
 public class 연속부분수열 {
-    public static int solution(int n, int m, int[] arr) {
-        int answer = 0;
-        int sum = 0;
-        int lt = 0;
-        for (int rt = 0; rt < n; rt++) {
-            sum += arr[rt];
-            if (sum == m) answer++;
-            while (sum >= m) {
+    public int solution(int N, int M, int[] arr){
+        int answer = 0, lt = 0, sum = 0;
+        for (int rt = 0; rt < N; rt++) {
+            if(sum < M) sum += arr[rt];
+            if(sum == M) answer++;
+            while(sum >= M) {
                 sum -= arr[lt++];
-                if (sum == m) answer++;
+                if(sum == M) answer ++;
             }
         }
         return answer;
     }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
-        System.out.println(solution(n, m, arr));
+        연속부분수열 main = new 연속부분수열();
+        Scanner kb = new Scanner(System.in);
+        int N = kb.nextInt();
+        int M = kb.nextInt();
+        int[] arr = new int[N];
+        for(int i = 0; i<N; i++) arr[i] = kb.nextInt();
+        System.out.print(main.solution(N,M,arr));
     }
 }

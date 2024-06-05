16c839acce307782d67e636d487a55421069eace
strongcookdas <goodjav141592@gmail.com>
2024-06-05 17:53:20 +0900

    chore : [인프런 복습] 해시 | 아나그램 | 10m | 1번

--- src/main/java/algorithm/hash/inflearn/아나그램.java
+++ src/main/java/algorithm/hash/inflearn/아나그램.java
@@ -3,32 +3,23 @@ package algorithm.hash.inflearn;
 import java.util.*;
 
 public class 아나그램 {
-    public static String solution(String str1, String str2) {
+    public String solution(String str1, String str2) {
         String answer = "YES";
-//        HashMap<Character, Integer> map1 = new HashMap<>();
-//        HashMap<Character, Integer> map2 = new HashMap<>();
-//        for (int i = 0; i < str1.length(); i++) {
-//            map1.put(str1.charAt(i), map1.getOrDefault(str1.charAt(i), 0) + 1);
-//            map2.put(str2.charAt(i), map2.getOrDefault(str2.charAt(i), 0) + 1);
-//        }
-//        for (char key : map1.keySet()) {
-//            if (!map2.containsKey(key) || !map2.get(key).equals(map1.get(key))) answer = "NO";
-//        }
+        if (str1.length() != str2.length()) return "NO";
         HashMap<Character, Integer> map = new HashMap<>();
-        for(char x : str1.toCharArray()){
-            map.put(x, map.getOrDefault(x,0)+1);
+        for (int i = 0; i < str1.length(); i++) {
+            map.put(str1.charAt(i), map.getOrDefault(str1.charAt(i), 0) + 1);
+            map.put(str2.charAt(i), map.getOrDefault(str2.charAt(i), 0) - 1);
         }
-        for(char key : str2.toCharArray()){
-            if(!map.containsKey(key) || map.get(key) == 0) return "NO";
-            map.put(key,map.get(key)-1);
+        for (Character key : map.keySet()) {
+            if (map.get(key) != 0) return "NO";
         }
         return answer;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String str1 = scanner.next();
-        String str2 = scanner.next();
-        System.out.println(solution(str1, str2));
+        Scanner kb = new Scanner(System.in);
+        아나그램 main = new 아나그램();
+        System.out.println(main.solution(kb.nextLine(), kb.nextLine()));
     }
 }

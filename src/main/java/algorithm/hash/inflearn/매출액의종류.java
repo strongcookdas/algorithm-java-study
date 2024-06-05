f932171603c51a814071b8f32421593adc1dd866
strongcookdas <goodjav141592@gmail.com>
2024-06-05 21:56:38 +0900

    chore : [인프런 복습] 해시 | 매출액의 종류 | 15m | 1번

--- src/main/java/algorithm/hash/inflearn/매출액의종류.java
+++ src/main/java/algorithm/hash/inflearn/매출액의종류.java
@@ -1,35 +1,36 @@
 package algorithm.hash.inflearn;
 
+/**
+ * 해시와 슬라이딩 윈도우 활용 (투 포인터)
+ */
+
 import java.util.*;
 
 public class 매출액의종류 {
-    public static ArrayList<Integer> solution(int n, int k, int[] arr) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
+    int N,K;
+    int[] arr;
+    public void solution(){
         int lt = 0;
-        for (int rt = 0; rt < n; rt++) {
-            if (rt < k) map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
-            else {
-                if (rt == k) answer.add(map.size());
-                if (map.get(arr[lt]) == 1) map.remove(arr[lt++]);
-                else map.put(arr[lt], map.get(arr[lt++]) - 1);
-                map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
-                answer.add(map.size());
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<N; i++){
+            if(i<K){
+                map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+            }else{
+                System.out.print(map.size()+" ");
+                if(map.get(arr[lt])==1) map.remove(arr[lt++]);
+                else map.put(arr[lt],map.get(arr[lt++])-1);
+                map.put(arr[i],map.getOrDefault(arr[i],0)+1);
             }
         }
-        return answer;
+        System.out.println(map.size());
     }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
-        for (Integer i : solution(n, k, arr)) {
-            System.out.print(i + " ");
-        }
+        Scanner kb = new Scanner(System.in);
+        매출액의종류 main = new 매출액의종류();
+        main.N = kb.nextInt();
+        main.K = kb.nextInt();
+        main.arr = new int[main.N];
+        for(int i = 0; i<main.N; i++)main.arr[i] = kb.nextInt();
+        main.solution();
     }
 }

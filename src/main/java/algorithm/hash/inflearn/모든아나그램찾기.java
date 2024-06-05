7bbfabcda5adb2e4dedb0f26f49d520414723b28
strongcookdas <goodjav141592@gmail.com>
2024-06-05 22:16:58 +0900

    chore : [인프런 복습] 해시 | 모든아나그램찾기 | 15m | 1번

--- src/main/java/algorithm/hash/inflearn/모든아나그램찾기.java
+++ src/main/java/algorithm/hash/inflearn/모든아나그램찾기.java
@@ -1,35 +1,34 @@
 package algorithm.hash.inflearn;
 
+/**
+ * 해시, 투포인터, 슬라이딩 윈도우, 이중 for문 사용
+ */
+
 import java.util.*;
 
-public class 모든아나그램찾기 { // 다시 풀기
-    public static int solution(String s, String t) {
-        int answer = 0;
-        HashMap<Character, Integer> am = new HashMap<>();
-        HashMap<Character, Integer> bm = new HashMap<>();
-        for (int i = 0; i < t.length(); i++) {
-            bm.put(t.charAt(i), bm.getOrDefault(t.charAt(i), 0) + 1);
+public class 모든아나그램찾기 {
+    String S,T;
+    public int solution(){
+        int N = T.length(), lt = 0, answer = 0;
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        HashMap<Character, Integer> tMap = new HashMap<>();
+        for(int i = 0; i<N; i++) tMap.put(T.charAt(i),tMap.getOrDefault(T.charAt(i),0)+1);
+        for(int i = 0; i<S.length(); i++){
+            if(i<N-1) sMap.put(S.charAt(i),sMap.getOrDefault(S.charAt(i),0)+1);
+            else{
+                sMap.put(S.charAt(i),sMap.getOrDefault(S.charAt(i),0)+1);
+                if(sMap.equals(tMap)) answer++;
+                if(sMap.get(S.charAt(lt))==1) sMap.remove(S.charAt(lt++));
+                else sMap.put(S.charAt(lt),sMap.get(S.charAt(lt++))-1);
+            }
         }
-        int lt = 0;
-        int rt = 0;
-        for (rt = 0; rt < t.length() - 1; rt++) {
-            am.put(s.charAt(rt), am.getOrDefault(s.charAt(rt), 0) + 1);
-        }
-
-        for (; rt < s.length(); rt++) {
-            am.put(s.charAt(rt), am.getOrDefault(s.charAt(rt), 0) + 1);
-            if (bm.equals(am)) answer++;
-            if (am.get(s.charAt(lt)) == 1) am.remove(s.charAt(lt++));
-            else am.put(s.charAt(lt), am.get(s.charAt(lt++)) - 1);
-        }
-
         return answer;
     }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
-        String t = scanner.nextLine();
-        System.out.println(solution(s, t));
+        Scanner kb = new Scanner(System.in);
+        모든아나그램찾기 main = new 모든아나그램찾기();
+        main.S = kb.nextLine();
+        main.T = kb.nextLine();
+        System.out.println(main.solution());
     }
 }

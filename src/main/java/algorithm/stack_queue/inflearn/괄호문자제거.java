d1b955b6db52a583e490b7e44a0481216b2e50d4
strongcookdas <goodjav141592@gmail.com>
2024-06-08 16:22:32 +0900

    chore : [인프런 복습] 스택 | 괄호문자 제거 | 5m | 1번

--- src/main/java/algorithm/stack_queue/inflearn/괄호문자제거.java
+++ src/main/java/algorithm/stack_queue/inflearn/괄호문자제거.java
@@ -2,19 +2,20 @@ package algorithm.stack_queue.inflearn;
 
 import java.util.*;
 
-public class 괄호문자제거 {
-    public static String solution(String str) {
+public class Main {
+    public static String solution(String str){
         StringBuilder sb = new StringBuilder("");
         Stack<Character> stack = new Stack<>();
-        for (char c : str.toCharArray()) {
-            if (c == ')') {
-                while (stack.pop() != '(') ;
-            } else stack.push(c);
+        for(char c : str.toCharArray()){
+            if(c==')'){
+                while(stack.pop()!='(');
+            }else if(c=='(') stack.push('(');
+            else{
+                if(stack.isEmpty()) sb.append(c);
+            }
         }
-        for (char c : stack) sb.append(c);
         return sb.toString();
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();

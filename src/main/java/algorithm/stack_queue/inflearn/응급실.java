de36eb90d63214f03931d09bb9aea42b0aa361d6
strongcookdas <goodjav141592@gmail.com>
2024-06-08 18:36:50 +0900

    chore : [인프런 복습] 큐 | 응급실 | 30m | 2번

--- src/main/java/algorithm/stack_queue/inflearn/응급실.java
+++ src/main/java/algorithm/stack_queue/inflearn/응급실.java
@@ -2,50 +2,45 @@ package algorithm.stack_queue.inflearn;
 
 import java.util.*;
 
-/*
-- 해당문제는 큐 말고도 정렬로 풀 수 있을 것 같았지만 같은 위험도를 가졌을 때 코드가 복잡해진다.
-- 아.. 큐 1개로는 같은 위험도 구분하기 어려움...
-- 큐 2개로 구현
-- 이건 java다... 큐 두개 만들 필요없이 클래스로 구현하면 된다.. (다시 보면 클래스로 해결하도록)
- */
 public class 응급실 {
-    public static int solution(int n, int m, int[] arr) { // 다시
-        int answer = 0;
-        Queue<Integer> idxQueue = new LinkedList<>();
-        Queue<Integer> riskQueue = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            idxQueue.offer(i);
+    static class Person implements Comparable<Person> {
+        int order;
+        int priority;
+
+        public Person(int order, int priority) {
+            this.order = order;
+            this.priority = priority;
         }
-        for (int i : arr) {
-            riskQueue.offer(i);
+
+        @Override
+        public int compareTo(Person o) {
+            return o.priority - this.priority;
         }
-        int idx = -1;
-        while (idx != m) {
-            boolean check = false;
-            for (Integer i : riskQueue) {
-                if (riskQueue.peek() < i) {
-                    idxQueue.offer(idxQueue.poll());
-                    riskQueue.offer(riskQueue.poll());
-                    check = true;
-                    break;
-                }
-            }
-            if (!check) {
-                answer++;
-                idx = idxQueue.poll();
-                riskQueue.poll();
-            }
+    }
+
+    public static int solution(int n, int m, Person[] people) {
+        Queue<Person> q = new LinkedList<>();
+        for (Person p : people) q.offer(p);
+        Arrays.sort(people);
+        int order = 1;
+        int idx = 0;
+        while (people[idx].priority > q.peek().priority || q.peek().order != m) {
+            if (q.peek().priority >= people[idx].priority) {
+                q.poll();
+                idx++;
+                order++;
+            } else q.offer(q.poll());
         }
-        return answer;
+        return order;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        int[] arr = new int[n];
+        Person[] arr = new Person[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
+            arr[i] = new Person(i, scanner.nextInt());
         }
         System.out.println(solution(n, m, arr));
     }

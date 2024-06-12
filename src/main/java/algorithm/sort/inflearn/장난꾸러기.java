3e215e12520acdc36a6791728118700879403abf
strongcookdas <goodjav141592@gmail.com>
2024-06-12 22:33:18 +0900

    chore : [인프런 복습] 정렬 | 장난꾸러기 | 5m | 1번

--- src/main/java/algorithm/sort/inflearn/장난꾸러기.java
+++ src/main/java/algorithm/sort/inflearn/장난꾸러기.java
@@ -1,15 +1,27 @@
 package algorithm.sort.inflearn;
 
+/**
+ * 투포인터로 풀어보려고 했지만 반례에 대한 처리가 미흡
+ * 그냥 정렬해서 풀었다.
+ */
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class 장난꾸러기 {
     public static void solution(int n, int[] arr) {
-        int[] tmp = arr.clone();
-        Arrays.sort(tmp);
-        for (int i = 0; i < n; i++) {
-            if (arr[i] != tmp[i]) System.out.print((i + 1) + " ");
+        int[] sortArr = arr.clone();
+        Arrays.sort(sortArr);
+
+        int a = -1, b = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != sortArr[i]) {
+                if (a == -1) a = i + 1;
+                else b = i + 1;
+            }
         }
+
+        System.out.println(a + " " + b);
     }
 
     public static void main(String[] argus) {

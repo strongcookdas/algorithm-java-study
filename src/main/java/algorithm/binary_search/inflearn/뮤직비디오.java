b58a849986b928a07905eb89dc3f0c6de58b769f
strongcookdas <goodjav141592@gmail.com>
2024-06-13 01:05:22 +0900

    chore : [인프런 복습] 이분 탐색 | 뮤직비디오 | 120m | 5번

--- src/main/java/algorithm/binary_search/inflearn/뮤직비디오.java
+++ src/main/java/algorithm/binary_search/inflearn/뮤직비디오.java
@@ -1,29 +1,32 @@
-package algorithm.sort.inflearn;
+package algorithm.binary_search.inflearn;
 
 import java.util.*;
 
-public class 뮤직비디오 {// 다시 풀어야하는 문제
-    public static int count(int[] arr, int capacity) {
-        int sum = 0, cnt = 1;
-        for (int i : arr) {
-            if (sum + i > capacity) {
-                cnt++;
-                sum = i;
-            } else sum += i;
+public class 뮤직비디오 {
+
+    public static int count(int[] arr, int cap) {
+        int sum = 0, count = 1;
+        for(int num : arr){
+            if(sum + num > cap){
+                count ++;
+                sum = num;
+            }else sum += num;
         }
-        return cnt;
+        return count;
     }
 
     public static int solution(int n, int m, int[] arr) {
-        int answer = 0;
         int lt = Arrays.stream(arr).max().getAsInt();
         int rt = Arrays.stream(arr).sum();
+        int answer = 0;
         while (lt <= rt) {
             int mid = (lt + rt) / 2;
-            if (count(arr, mid) <= m) {
+            if (count(arr,mid)<=m) {
                 answer = mid;
                 rt = mid - 1;
-            } else lt = mid + 1;
+            } else {
+                lt = mid + 1;
+            }
         }
         return answer;
     }

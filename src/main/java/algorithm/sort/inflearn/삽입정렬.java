6c99ed047f8e3dae192c2b584322cabb0f88000f
strongcookdas <goodjav141592@gmail.com>
2024-06-12 18:29:08 +0900

    chore : [인프런 복습] 정렬 | 삽입 정렬 | 10m | 1번

--- src/main/java/algorithm/sort/inflearn/삽입정렬.java
+++ src/main/java/algorithm/sort/inflearn/삽입정렬.java
@@ -3,15 +3,16 @@ package algorithm.sort.inflearn;
 import java.util.Scanner;
 
 public class 삽입정렬 {
-    public static int[] solution(int n, int[] arr) {
-        for (int i = 1; i < n; i++) {
-            int tmp = arr[i];
-            int j;
-            for (j = i - 1; j >= 0; j--) {
-                if (arr[j] > tmp) arr[j + 1] = arr[j];
-                else break;
+
+    public static int[] solution(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j + 1] < arr[j]) {
+                    int tmp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = tmp;
+                }else break;
             }
-            arr[j+1] = tmp;
         }
         return arr;
     }
@@ -21,6 +22,6 @@ public class 삽입정렬 {
         int n = scanner.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) arr[i] = scanner.nextInt();
-        for (int i : solution(n, arr)) System.out.print(i + " ");
+        for (int i : solution(arr)) System.out.print(i + " ");
     }
 }

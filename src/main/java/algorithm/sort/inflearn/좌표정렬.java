4de8c0fff602229c9ec934d0022c100757b89cbf
strongcookdas <goodjav141592@gmail.com>
2024-06-12 22:39:34 +0900

    chore : [인프런 복습] 정렬 | 좌표 정렬 | 5m | 1번

--- src/main/java/algorithm/sort/inflearn/좌표정렬.java
+++ src/main/java/algorithm/sort/inflearn/좌표정렬.java
@@ -2,29 +2,25 @@ package algorithm.sort.inflearn;
 
 import java.util.*;
 
-class Point implements Comparable<Point> {
-    int x;
-    int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public int compareTo(Point o) {
-        if (this.x == o.x) return this.y - o.y;
-        else return this.x - o.x;
-    }
-}
-
 public class 좌표정렬 {
+    static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     public static void main(String[] argus) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         ArrayList<Point> arr = new ArrayList<>();
         for (int i = 0; i < n; i++) arr.add(new Point(scanner.nextInt(), scanner.nextInt()));
-        Collections.sort(arr);
+        Collections.sort(arr, (a, b) -> {
+            if(a.x==b.x) return a.y-b.y;
+            return a.x-b.x;
+        });
         for (Point c : arr) System.out.println(c.x + " " + c.y);
     }
 }

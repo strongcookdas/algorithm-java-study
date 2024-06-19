8df9d4eab64f06e6f9307ec03e81cd5fcdd37a20
strongcookdas <goodjav141592@gmail.com>
2024-06-19 18:03:15 +0900

    chore : [인프런 복습] 그리디 | 결혼식 | 30m | 2번

--- src/main/java/algorithm/greedy/inflearn/결혼식.java
+++ src/main/java/algorithm/greedy/inflearn/결혼식.java
@@ -1,45 +1,54 @@
 package algorithm.greedy.inflearn;
 
+// 입력을 받는 형식이 중요
+// 나가는 시간과 들어오는 시간을 구분하여 입력을 받아야 한다.
+// Time 이라는 객체 생성 (시간과 나가는 시간인지 들어오는 시간이지 구분하는 필드)
+
 import java.util.*;
 
-class Time implements Comparable<Time> {
-    public int time;
-    public char state;
-
-    public Time(int time, char state) {
-        this.time = time;
-        this.state = state;
-    }
-    @Override
-    public int compareTo(Time o) {
-        if (this.time == o.time) return this.state - o.state;
-        return this.time - o.time;
-    }
-}
-
 public class 결혼식 {
-    public static int solution(ArrayList<Time> arr) {
-        int answer = Integer.MIN_VALUE;
-        Collections.sort(arr);
-        int cnt = 0;
-        for(Time ob : arr){
-            if(ob.state == 's') cnt++;
-            else cnt--;
-            answer=Math.max(answer, cnt);
+    static class Time {
+        int time;
+        int tmp; // 0이면 입장 , 1이면 퇴장
+
+        public Time(int time, int tmp) {
+            this.time = time;
+            this.tmp = tmp;
         }
-        return answer;
     }
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
-        ArrayList<Time> arr = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            int sT = kb.nextInt();
-            int eT = kb.nextInt();
-            arr.add(new Time(sT, 's'));
-            arr.add(new Time(eT, 'e'));
+        int N = kb.nextInt();
+        ArrayList<Time> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(new Time(kb.nextInt(), 0));
+            list.add(new Time(kb.nextInt(), 1));
         }
-        System.out.println(solution(arr));
+
+        결혼식 main = new 결혼식();
+        System.out.println(main.solution(list));
+    }
+
+    private int solution(ArrayList<Time> list) {
+        list.sort((a, b) -> {
+            if (a.time == b.time) {
+                return b.tmp - a.tmp;
+            }
+            return a.time - b.time;
+        });
+
+        int count = 0;
+        int answer = 0;
+        for (Time t : list) {
+            if (t.tmp == 1) {
+                count--;
+            } else {
+                count++;
+                answer = Math.max(count, answer);
+            }
+        }
+        return answer;
     }
 }

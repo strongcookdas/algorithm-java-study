ef8eb844c5311ff39e2194778dbf1ffc3f29bd82
strongcookdas <goodjav141592@gmail.com>
2024-07-02 01:00:41 +0900

    chore : [인프런 복습] 배열 | 큰수출력하기 | 3m | 1번

--- src/main/java/algorithm/array/inflearn/큰수출력하기.java
+++ src/main/java/algorithm/array/inflearn/큰수출력하기.java
@@ -3,24 +3,20 @@ package algorithm.array.inflearn;
 import java.util.*;
 
 public class 큰수출력하기 {
-    public static ArrayList<Integer> solution(int n, int[] arr) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        answer.add(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            if(arr[i]>arr[i-1]) answer.add(arr[i]);
-        }
-        return answer;
-    }
+    static int[] arr;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = scanner.nextInt();
+        Scanner kb = new Scanner(System.in);
+        int N = kb.nextInt();
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = kb.nextInt();
         }
-        for (Integer i : solution(n, arr)) {
-            System.out.print(i + " ");
+
+        for (int i = 0; i < N; i++) {
+            if (i == 0 || arr[i] > arr[i - 1]) {
+                System.out.print(arr[i] + " ");
+            }
         }
     }
 }

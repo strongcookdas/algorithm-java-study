85c5e1ffff9ea85c6cd6bbb9ce425011360f1dd8
strongcookdas <goodjav141592@gmail.com>
2024-07-01 15:18:46 +0900

    chore : [인프런 복습] dp | 계단 오르기 | 5m | 1번

--- src/main/java/algorithm/dynamic/inflearn/계단오르기.java
+++ src/main/java/algorithm/dynamic/inflearn/계단오르기.java
@@ -3,21 +3,21 @@ package algorithm.dynamic.inflearn;
 import java.util.*;
 
 public class 계단오르기 {
-    static int[] dy;
-
-    public static int solution(int n) {
-        dy[1] = 1;
-        dy[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dy[i] = dy[i - 1] + dy[i - 2];
-        }
-        return dy[n];
-    }
+    static int[] dp;
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
-        dy = new int[n + 1];
-        System.out.println(solution(n));
+        int N = kb.nextInt();
+        System.out.println(solution(N));
+    }
+
+    private static int solution(int n) {
+        dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+        return dp[n];
     }
 }

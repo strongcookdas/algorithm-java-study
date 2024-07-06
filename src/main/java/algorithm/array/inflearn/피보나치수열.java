83058f9aaae677332ef423ce8f16d6cec17ff661
strongcookdas <goodjav141592@gmail.com>
2024-07-06 18:18:06 +0900

    chore : [인프런 복습] 배열 | 피보나치 수열 | 5m | 1번

--- src/main/java/algorithm/array/inflearn/피보나치수열.java
+++ src/main/java/algorithm/array/inflearn/피보나치수열.java
@@ -4,22 +4,16 @@ import java.util.*;
 
 public class 피보나치수열 {
     public static void solution(int n) {
-//        int[] answer = new int[n];
-//        answer[0] = 1;
-//        answer[1] = 1;
-//        for (int i = 2; i < n; i++) {
-//            answer[i] = answer[i - 1] + answer[i - 2];
-//        }
-        int a = 1, b = 1;
+        int[] fibo = new int[n];
         for (int i = 0; i < n; i++) {
             if (i == 0 || i == 1) {
-                System.out.print(1 + " ");
-                continue;
+                fibo[i] = 1;
+            } else {
+                fibo[i] = fibo[i - 1] + fibo[i - 2];
             }
-            int c = a + b;
-            System.out.print(c + " ");
-            a = b;
-            b = c;
+        }
+        for (int i : fibo) {
+            System.out.print(i + " ");
         }
     }
 

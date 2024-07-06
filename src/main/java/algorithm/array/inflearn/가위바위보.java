306ad5fb23d969e02da8515b18600e515fe0fdae
strongcookdas <goodjav141592@gmail.com>
2024-07-06 18:12:51 +0900

    chore : [인프런 복습] 배열 | 가위 바위 보 | 50m | 1번

--- src/main/java/algorithm/array/inflearn/가위바위보.java
+++ src/main/java/algorithm/array/inflearn/가위바위보.java
@@ -6,11 +6,15 @@ public class 가위바위보 {
     public static ArrayList<String> solution(int[] arr1, int[] arr2) {
         ArrayList<String> answer = new ArrayList<>();
         for (int i = 0; i < arr1.length; i++) {
-            if(arr1[i] == arr2[i]) answer.add("D");
-            else if(arr1[i]==1 && arr2[i]==3) answer.add("A");
-            else if(arr1[i]==2 && arr2[i]==1) answer.add("A");
-            else if(arr1[i]==3 && arr2[i]==2) answer.add("A");
-            else answer.add("B");
+            int a = arr1[i];
+            int b = arr2[i];
+            if ((a == 1 && b == 3) || (a == 2 && b == 1) || (a == 3 && b == 2)) {
+                answer.add("A");
+            } else if (a == b) {
+                answer.add("D");
+            } else {
+                answer.add("B");
+            }
         }
         return answer;
     }
@@ -26,7 +30,7 @@ public class 가위바위보 {
         for (int i = 0; i < n; i++) {
             arr2[i] = scanner.nextInt();
         }
-        ArrayList<String> answer = solution(arr1,arr2);
+        ArrayList<String> answer = solution(arr1, arr2);
         for (String s : answer) {
             System.out.println(s);
         }

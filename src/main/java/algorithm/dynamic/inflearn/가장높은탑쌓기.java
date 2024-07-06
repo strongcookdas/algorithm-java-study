12355a85bc6249ef81edc511be135030864b2856
strongcookdas <goodjav141592@gmail.com>
2024-07-06 18:02:01 +0900

    chore : [인프런 복습] dp | 가장 높은 탑 쌓기 | 30m | 1번

--- src/main/java/algorithm/dynamic/inflearn/가장높은탑쌓기.java
+++ src/main/java/algorithm/dynamic/inflearn/가장높은탑쌓기.java
@@ -2,46 +2,48 @@ package algorithm.dynamic.inflearn;
 
 import java.util.*;
 
-class Brick implements Comparable<Brick>{
-    int width;
-    int height;
-    int weight;
-
-    public Brick(int width, int height, int weight){
-        this.width = width;
-        this.height = height;
-        this.weight = weight;
-    }
-
-    @Override
-    public int compareTo(Brick o) {
-        return o.width - this.width;
-    }
-}
 public class 가장높은탑쌓기 {
-    static int[] dy;
 
-    public static int solution(ArrayList<Brick> list){
-        int answer = 0;
-        Collections.sort(list);
-        dy[0] = list.get(0).height;
-        answer = dy[0];
-        for(int i =1; i<list.size(); i++){
-            int max = 0;
-            for(int j = i-1; j>=0; j--){
-                if(list.get(i).weight<list.get(j).weight && dy[j]>max) max = dy[j];
-            }
-            dy[i] = max + list.get(i).height;
-            answer = Math.max(answer, dy[i]);
+    static class Brick {
+        int width;
+        int height;
+        int weight;
+
+        public Brick(int width, int height, int weight) {
+            this.width = width;
+            this.height = height;
+            this.weight = weight;
         }
-        return answer;
     }
-    public static void main(String[] args){
+
+    static Brick[] inputs;
+    static int[] dp;
+    static int answer = Integer.MIN_VALUE;
+
+    public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
-        dy = new int[n];
-        ArrayList<Brick> list = new ArrayList<>();
-        for(int i = 0; i<n; i++) list.add(new Brick(kb.nextInt(),kb.nextInt(),kb.nextInt()));
-        System.out.println(solution(list));
+        int N = kb.nextInt();
+        dp = new int[N];
+        inputs = new Brick[N];
+
+        for (int i = 0; i < N; i++) {
+            inputs[i] = new Brick(kb.nextInt(), kb.nextInt(), kb.nextInt());
+        }
+
+        solution();
+        System.out.println(answer);
+    }
+
+    private static void solution() {
+        Arrays.sort(inputs, (a, b) -> b.width - a.width);
+        for (int i = 0; i < inputs.length; i++) {
+            dp[i] = inputs[i].height;
+            for (int j = 0; j < i; j++) {
+                if (inputs[i].weight <= inputs[j].weight) {
+                    dp[i] = Math.max(dp[i], inputs[i].height + dp[j]);
+                }
+            }
+            answer = Math.max(answer, dp[i]);
+        }
     }
 }

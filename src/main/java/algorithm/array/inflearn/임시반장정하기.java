cff8373a1039c2336926f5ae2af489bc18d69c47
strongcookdas <goodjav141592@gmail.com>
2024-07-06 21:20:56 +0900

    chore : [인프런 복습] 배열 | 임시반장 정하기 | 10m | 1번

--- src/main/java/algorithm/array/inflearn/임시반장정하기.java
+++ src/main/java/algorithm/array/inflearn/임시반장정하기.java
@@ -4,26 +4,28 @@ import java.util.*;
 
 public class 임시반장정하기 {
     public static int solution(int n, int[][] arr) {
-        int answer = 0;
-        int max = 0;
+        int answer = 0, max = Integer.MIN_VALUE;
+        int[] ch = new int[n];
         for (int i = 0; i < n; i++) {
-            int[] flag = new int[n];
             int count = 0;
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < 5; k++) {
-                    if (arr[i][k] == arr[j][k]) {
-                        if (flag[j] == 1) continue;
-                        flag[j] = 1;
+            Arrays.fill(ch, 0);
+            for (int j = 0; j < 5; j++) {
+                for (int z = 0; z < n; z++) {
+                    if (z == i) {
+                        continue;
+                    }
+                    if (arr[i][j] == arr[z][j] && ch[z] == 0) {
                         count++;
+                        ch[z] = 1;
                     }
                 }
             }
-            if (max < count) {
+            if (count > max) {
                 max = count;
-                answer = i;
+                answer = i + 1;
             }
         }
-        return answer+1;
+        return answer;
     }
 
     public static void main(String[] args) {

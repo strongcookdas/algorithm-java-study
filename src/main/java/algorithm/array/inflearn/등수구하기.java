a269a0fd5b355eebc271b3fafa75e48040017bf6
strongcookdas <goodjav141592@gmail.com>
2024-07-06 19:49:33 +0900

    chore : [인프런 복습] 배열 | 등수 구하기 | 10m | 1번

--- src/main/java/algorithm/array/inflearn/등수구하기.java
+++ src/main/java/algorithm/array/inflearn/등수구하기.java
@@ -5,12 +5,21 @@ import java.util.*;
 public class 등수구하기 {
     public static int[] solution(int[] arr) {
         int[] answer = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            int count = 1;
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[i] < arr[j]) count++;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] arrCopy = arr.clone();
+        Arrays.sort(arrCopy);
+        int count = 1;
+        int before = -1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arrCopy[i] != before) {
+                before = arrCopy[i];
+                map.put(arrCopy[i], count);
             }
-            answer[i] = count;
+            count++;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            answer[i] = map.get(arr[i]);
         }
         return answer;
     }

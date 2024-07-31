395361f3274ba9df8ab7d31b991093369ed3c055
strongcookdas <goodjav141592@gmail.com>
2024-08-01 02:32:17 +0900

    chore : [백준] 수열 | N과M11 | S2 | 5m | 1번

---
+++ src/main/java/algorithm/permutation/boj/N과M15665.java
@@ -0,0 +1,61 @@
+package algorithm.permutation.boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class N과M15665 {
+    int[] inputs;
+    int[] seq;
+    HashSet<String> visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N과M15665 main = new N과M15665();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        main.inputs = new int[N];
+        main.seq = new int[M];
+        main.visited = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            main.inputs[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(main.inputs);
+        main.solution(0, bw);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private void solution(int L, BufferedWriter bw) throws IOException {
+        if (L == seq.length) {
+            StringBuilder sb = new StringBuilder("");
+            for (int n : seq) {
+                sb.append(n);
+                sb.append(" ");
+            }
+            sb.append("\n");
+            if (!visited.contains(sb.toString())) {
+                visited.add(sb.toString());
+                bw.write(sb.toString());
+            }
+        } else {
+            for (int i = 0; i < inputs.length; i++) {
+                seq[L] = inputs[i];
+                solution(L + 1, bw);
+            }
+        }
+    }
+}

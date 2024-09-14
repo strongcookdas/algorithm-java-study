2ff09c0ea9c3de8a58863586792b19cafcc29391
strongcookdas <goodjav141592@gmail.com>
2024-09-15 08:58:35 +0900

    chore : [백준] 기하 | 직사각형 | B5 | 5m | 1번

---
+++ src/main/java/algorithm/geometry/boj/직사각형27323.java
@@ -0,0 +1,13 @@
+package algorithm.geometry.boj;
+
+import java.util.*;
+public class 직사각형27323 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        int A = kb.nextInt();
+        int B = kb.nextInt();
+
+        System.out.println(A*B);
+    }
+}

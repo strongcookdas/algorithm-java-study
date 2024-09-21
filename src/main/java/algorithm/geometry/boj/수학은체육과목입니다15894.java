c0cd9ce27042265502a271922f090204f87f3d4f
strongcookdas <goodjav141592@gmail.com>
2024-09-21 17:53:12 +0900

    chore : [백준] 기하 | 수학은 체육과목입니다. | B3 | 5m | 1번

---
+++ src/main/java/algorithm/geometry/boj/수학은체육과목입니다15894.java
@@ -0,0 +1,12 @@
+package algorithm.geometry.boj;
+
+import java.util.*;
+
+public class 수학은체육과목입니다15894 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        long n = kb.nextLong();
+        long answer = n * 4;
+        System.out.println(answer);
+    }
+}

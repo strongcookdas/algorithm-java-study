f5a10c6acc7fd8e7e038c054e84ce3f3e8f2cccb
strongcookdas <goodjav141592@gmail.com>
2024-10-31 00:37:50 +0900

    chore : [PG] 힙 | 더 맵게 | L2 | 15M

---
+++ src/main/java/algorithm/heap/programmers/더맵게.java
@@ -0,0 +1,23 @@
+package algorithm.heap.programmers;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pQ = new PriorityQueue<>();
+        for(int n : scoville){
+            pQ.offer(n);
+        }
+        while(pQ.peek() < K && pQ.size()>1 ){
+            int s1 = pQ.poll();
+            int s2 = pQ.poll() * 2;
+            pQ.offer(s1+s2);
+            answer++;
+        }
+        if(pQ.peek() >= K){
+            return answer;
+        }
+        return -1;
+    }
+}

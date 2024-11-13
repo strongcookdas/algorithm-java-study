83103c90e48df47d1563e2c295de7385e0bbf454
strongcookdas <goodjav141592@gmail.com>
2024-11-13 10:00:46 +0900

    chore : [PG] 수학 | 평균구하기 | L1 | 1M

---
+++ src/main/java/algorithm/math/programmers/평균구하기.java
@@ -0,0 +1,11 @@
+package algorithm.math.programmers;
+
+public class 평균구하기 {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int n : arr){
+            answer+=n;
+        }
+        return answer/arr.length;
+    }
+}

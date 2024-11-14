a8feb1eb2dc4b7b18ad6cb3495e0504a913acd27
strongcookdas <goodjav141592@gmail.com>
2024-11-14 10:05:19 +0900

    chore : [PG] 수학 | 자릿수더하기 | L0 | 1M

---
+++ src/main/java/algorithm/math/programmers/자릿수더하기.java
@@ -0,0 +1,12 @@
+package algorithm.math.programmers;
+
+public class 자릿수더하기 {
+    public int solution(int n) {
+        int answer = 0;
+        while(n>0){
+            answer += (n%10);
+            n/=10;
+        }
+        return answer;
+    }
+}

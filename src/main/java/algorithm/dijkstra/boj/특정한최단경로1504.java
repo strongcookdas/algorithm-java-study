86ac1c7f9a419a3c6bddb2fb7b8dbad122df583d
strongcookdas <goodjav141592@gmail.com>
2024-11-25 13:12:01 +0900

    chore : [백준] 다익스트라 | 특정한 최단 경로 1504 | G2 | 30M

--- src/main/java/algorithm/dijkstra/boj/특정한최단경로1504.java
+++ src/main/java/algorithm/dijkstra/boj/특정한최단경로1504.java
@@ -3,12 +3,8 @@ package algorithm.dijkstra.boj;
 import java.util.*;
 import java.io.*;
 
-// S -> A -> B -> D
-// S -> B -> A -> D
 public class 특정한최단경로1504 {
-    static final int INF = 200000000;
-
-    public static class Node implements Comparable<Node> {
+    static class Node implements Comparable<Node> {
         int node;
         int cost;
 
@@ -21,28 +17,27 @@ public class 특정한최단경로1504 {
         public int compareTo(Node o) {
             return this.cost - o.cost;
         }
-
     }
 
-    public ArrayList<ArrayList<Node>> graph;
+    ArrayList<ArrayList<Node>> graph;
+    int[] dis;
 
     public 특정한최단경로1504(int N) {
-        graph = new ArrayList<>();
+        this.dis = new int[N + 1];
+        this.graph = new ArrayList<>();
         for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
+            this.graph.add(new ArrayList<>());
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
 
         특정한최단경로1504 main = new 특정한최단경로1504(N);
-
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
@@ -53,55 +48,65 @@ public class 특정한최단경로1504 {
         }
 
         st = new StringTokenizer(br.readLine());
-        int v1 = Integer.parseInt(st.nextToken());
-        int v2 = Integer.parseInt(st.nextToken());
-        bw.write(main.solution(v1, v2));
-        bw.flush();
-        bw.close();
-        br.close();
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+
+        System.out.println(main.solution(N, n1, n2));
     }
 
-    public String solution(int v1, int v2) {
-        int[] disFromSource = getDistance(1);
-        int[] disFromV1 = getDistance(v1);
-        int[] disFromV2 = getDistance(v2);
+    public int solution(int n, int n1, int n2) {
+        int p1 = getDis(1, n1);
+        int p2 = getDis(n1, n2);
+        int p3 = getDis(n2, n);
 
-        int path1 = disFromSource[v1] + disFromV1[v2] + disFromV2[graph.size() - 1];
-        int path2 = disFromSource[v2] + disFromV2[v1] + disFromV1[graph.size() - 1];
+        int p4 = getDis(1, n2);
+        int p5 = getDis(n2, n1);
+        int p6 = getDis(n1, n);
 
-        int answer = Math.min(path1, path2);
-        if (answer >= INF) {
-            return String.valueOf(-1);
+        int dis1, dis2;
+        if (p1 == -1 || p2 == -1 || p3 == -1) {
+            dis1 = -1;
         } else {
-            return String.valueOf(answer);
+            dis1 = p1 + p2 + p3;
         }
+
+        if (p3 == -1 || p4 == -1 || p5 == -1) {
+            dis2 = -1;
+        } else {
+            dis2 = p4 + p5 + p6;
+        }
+
+        if (dis1 == -1) {
+            return dis2;
+        } else if (dis2 == -1) {
+            return dis1;
+        }
+
+        return Math.min(dis1, dis2);
     }
 
-    public int[] getDistance(int from) {
-        int[] dis = new int[graph.size()];
-        Arrays.fill(dis, INF);
-        dis[from] = 0;
-
+    public int getDis(int s, int d) {
+        Arrays.fill(this.dis, Integer.MAX_VALUE);
+        this.dis[s] = 0;
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(from, 0));
-
+        pq.offer(new Node(s, 0));
         while (!pq.isEmpty()) {
             Node tmp = pq.poll();
-            int cost = dis[tmp.node];
-
-            if (cost > tmp.cost) {
+            int now = tmp.node;
+            int nowCost = tmp.cost;
+            if (now == d) {
+                return nowCost;
+            }
+            if (this.dis[now] < nowCost) {
                 continue;
             }
-
-            for (Node d : graph.get(tmp.node)) {
-                int dCost = cost + d.cost;
-                if (dis[d.node] > dCost) {
-                    dis[d.node] = dCost;
-                    pq.add(new Node(d.node, dis[d.node]));
+            for (Node node : this.graph.get(now)) {
+                if (dis[node.node] > nowCost + node.cost) {
+                    dis[node.node] = nowCost + node.cost;
+                    pq.offer(new Node(node.node, dis[node.node]));
                 }
             }
         }
-
-        return dis;
+        return -1;
     }
 }

94ad7533dddbc61f93ac4a374491a645d7d5aa37
strongcookdas <goodjav141592@gmail.com>
2024-11-28 10:54:05 +0900

    chore : [백준] BFS | 숨바꼭질13549 | G5 | 30M

--- src/main/java/algorithm/bfs/boj/숨바꼭질13549.java
+++ src/main/java/algorithm/bfs/boj/숨바꼭질13549.java
@@ -1,52 +1,47 @@
 package algorithm.bfs.boj;
 
+//https://www.acmicpc.net/problem/13549
+
 import java.util.*;
 
 public class 숨바꼭질13549 {
-    static class Position {
-        int x;
-        int time;
-
-        public Position(int x, int time) {
-            this.x = x;
-            this.time = time;
-        }
-    }
-
     public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
-        int N = kb.nextInt();
-        int K = kb.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
 
         숨바꼭질13549 main = new 숨바꼭질13549();
-        System.out.println(main.solution(N, K));
+        System.out.println(main.solution(n, k));
     }
 
-    private int solution(int n, int k) {
-        Queue<Position> q = new LinkedList<>();
-        HashMap<Integer, Integer> visited = new HashMap<>();
-        q.offer(new Position(n, 0));
-        visited.put(n, 0);
-
+    public int solution(int n, int k) {
+        Queue<Integer> q = new LinkedList<>();
+        int[] time = new int[100005];
+        Arrays.fill(time, -1);
+        q.offer(n);
+        time[n] = 0;
         while (!q.isEmpty()) {
             int size = q.size();
             for (int i = 0; i < size; i++) {
-                Position p = q.poll();
-                if (p.x == k) {
-                    return p.time;
+                int now = q.poll();
+                if (now == k) {
+                    return time[now];
                 }
-                int[] move = {p.x * 2, p.x - 1, p.x + 1};
-                int[] time = {p.time, p.time + 1, p.time + 1};
-                for (int j = 0; j < move.length; j++) {
-                    int nx = move[j];
-                    int nTime = time[j];
-                    if (nx >= 0 && nx <= 100000 && (!visited.containsKey(nx) || visited.get(nx) > nTime)) {
-                        visited.put(nx, nTime);
-                        q.offer(new Position(nx, nTime));
-                    }
+                if (now * 2 >= 0 && now * 2 < time.length && time[now * 2] == -1) {
+                    time[now * 2] = time[now];
+                    q.offer(now * 2);
+                }
+                if (now - 1 >= 0 && now - 1 < time.length && time[now - 1] == -1) {
+                    time[now - 1] = time[now] + 1;
+                    q.offer(now - 1);
+                }
+                if (now + 1 >= 0 && now + 1 < time.length && time[now + 1] == -1) {
+                    time[now + 1] = time[now] + 1;
+                    q.offer(now + 1);
                 }
             }
         }
-        return 0;
+        return -1;
     }
+
 }

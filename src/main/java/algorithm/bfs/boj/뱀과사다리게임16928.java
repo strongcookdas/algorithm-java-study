2a4805960109d06a2d1bd976a9dde6b7f4d1804a
strongcookdas <goodjav141592@gmail.com>
2024-12-19 12:19:37 +0900

    chore : [BOJ] BFS | 뱀과 사다리 게임 16928 | 60M

--- src/main/java/algorithm/bfs/boj/뱀과사다리게임16928.java
+++ src/main/java/algorithm/bfs/boj/뱀과사다리게임16928.java
@@ -1,27 +1,32 @@
 package algorithm.bfs.boj;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class 뱀과사다리게임16928 {
-    HashMap<Integer, Integer> ladder;
-    HashMap<Integer, Integer> snake;
+
+    Map<Integer, Integer> ladder;
+    Map<Integer, Integer> snake;
+    int[] map;
 
     public 뱀과사다리게임16928() {
         ladder = new HashMap<>();
         snake = new HashMap<>();
+        map = new int[101];
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         뱀과사다리게임16928 main = new 뱀과사다리게임16928();
-
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
@@ -31,48 +36,49 @@ public class 뱀과사다리게임16928 {
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            main.snake.put(u, v);
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            main.snake.put(x, y);
         }
 
-        bw.write(main.solution());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(main.bfs());
+
     }
 
-    private String solution() {
-        int[] ch = new int[101];
+    public int bfs() {
         Queue<Integer> q = new LinkedList<>();
-        int start = 1;
-        ch[1] = 1;
-
-        q.offer(start);
+        q.offer(1);
+        map[1] = 1;
+        int count = 0;
         while (!q.isEmpty()) {
             int size = q.size();
             for (int i = 0; i < size; i++) {
-                int idx = q.poll();
-                if (idx == 100) {
-                    return String.valueOf(ch[idx] - 1);
+                int n = q.poll();
+                if (n == 100) {
+                    return count;
                 }
                 for (int j = 1; j <= 6; j++) {
-                    int nextIdx = idx + j;
-                    if (nextIdx <= 100 && ch[nextIdx] == 0) {
-                        if (ladder.containsKey(nextIdx)) {
-                            nextIdx = ladder.get(nextIdx);
-                        } else if (snake.containsKey(nextIdx)) {
-                            nextIdx = snake.get(nextIdx);
-                        }
-
-                        if (ch[nextIdx] == 0) {
-                            ch[nextIdx] = ch[idx] + 1;
-                            q.offer(nextIdx);
-                        }
+                    int nextN = n + j;
+                    if (nextN > 100 || map[nextN] != 0) {
+                        continue;
                     }
+                    if (ladder.containsKey(nextN)) {
+                        nextN = ladder.get(nextN);
+                    } else if (snake.containsKey(nextN)) {
+                        nextN = snake.get(nextN);
+                    }
+                    map[nextN] = 1;
+                    q.add(nextN);
                 }
             }
+            count++;
         }
-        return String.valueOf(0);
+        return -1;
     }
 }
+
+/*
+시간 복잡도 : O(600) 100*6
+시간 : 120ms
+메모리 : 14680kb
+ */

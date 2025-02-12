d89189f9ff6a7fba28fff03f8f50fa94f172041e
strongcookdas <goodjav141592@gmail.com>
2025-02-12 18:14:06 +0900

    feat : [BOJ] 완전탐색 | 사탕게임 3085 | 30m

--- src/main/java/algorithm/brute_force/boj/사탕게임3085.java
+++ src/main/java/algorithm/brute_force/boj/사탕게임3085.java
@@ -1,98 +1,73 @@
 package algorithm.brute_force.boj;
 
-// swap 후 해당 열과 행을 체크
-// 시간 복잡도 50*50*4*50 1억을 넘지 않음
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class 사탕게임3085 {
-    char[][] board;
-    int[] dx = {-1, 0}, dy = {0, 1};
+    static char[][] board;
+    static int ans=Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-
-        사탕게임3085 main = new 사탕게임3085();
-        main.board = new char[N][N];
-        for (int i = 0; i < N; i++) {
-            String input = br.readLine();
-            for (int j = 0; j < N; j++) {
-                main.board[i][j] = input.charAt(j);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        board = new char[n][n];
+        for(int i = 0; i<n; i++){
+            String str = sc.nextLine();
+            for(int j = 0; j<n; j++){
+                board[i][j] = str.charAt(j);
             }
         }
 
-        bw.write(String.valueOf(main.solution()));
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                if(i+1 < n){
+                    swap(i,j,i+1,j);
+                    compare(i,j);
+                    compare(i+1,j);
+                    swap(i,j,i+1,j);
+                }
 
-    private int solution() {
-        int answer = Integer.MIN_VALUE;
-
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                answer = Math.max(answer, swap(j, i));
+                if(j+1 < n){
+                    swap(i,j,i,j+1);
+                    compare(i,j);
+                    compare(i,j+1);
+                    swap(i,j,i,j+1);
+                }
             }
         }
-        return answer;
+
+        System.out.println(ans);
+
     }
 
-    private int swap(int x, int y) {
-        int answer = 0;
-        for (int i = 0; i < dx.length; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && nx < board.length && ny >= 0 && ny < board.length) {
-                char tmp = board[y][x];
-                board[y][x] = board[ny][nx];
-                board[ny][nx] = tmp;
-
-                answer = Math.max(Math.max(compareCandy(x, y, board[y][x]), compareCandy(nx, ny, board[ny][nx])),
-                        answer);
-
-                tmp = board[y][x];
-                board[y][x] = board[ny][nx];
-                board[ny][nx] = tmp;
-            }
-        }
-        return answer;
+    public static void swap(int x1, int y1, int x2, int y2){
+        char tmp = board[x1][y1];
+        board[x1][y1] = board[x2][y2];
+        board[x2][y2] = tmp;
     }
 
-    private int compareCandy(int x, int y, char c) {
+    public static void compare(int x, int y){
         int row = 1;
-        for (int lt = x - 1; lt >= 0; lt--) {
-            if (board[y][lt] != c) {
-                break;
+        for(int i = 1; i<board.length; i++){
+            if(board[x][i] == board[x][i-1]) {
+                row++;
+            }else {
+                ans = Math.max(ans, row);
+                row = 1;
             }
-            row++;
         }
-        for (int rt = x + 1; rt < board.length; rt++) {
-            if (board[y][rt] != c) {
-                break;
-            }
-            row++;
-        }
+        ans = Math.max(ans, row);
 
         int col = 1;
-        for (int lt = y - 1; lt >= 0; lt--) {
-            if (board[lt][x] != c) {
-                break;
+        for(int i = 1; i<board.length; i++){
+            if(board[i][y] == board[i-1][y]) {
+                col++;
+            }else{
+                ans = Math.max(ans, col);
+                col = 1;
             }
-            col++;
         }
-        for (int rt = y + 1; rt < board.length; rt++) {
-            if (board[rt][x] != c) {
-                break;
-            }
-            col++;
-        }
-        return Math.max(row, col);
+        ans = Math.max(ans, col);
     }
 }

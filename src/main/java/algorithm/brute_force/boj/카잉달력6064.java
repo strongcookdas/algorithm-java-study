452d1ac5b2c1c760069a2d236f6a0f4fbf1ee4da
strongcookdas <goodjav141592@gmail.com>
2025-02-12 19:47:26 +0900

    feat : [BOJ] 완전탐색 | 카잉달력 6064 | 10m

--- src/main/java/algorithm/brute_force/boj/카잉달력6064.java
+++ src/main/java/algorithm/brute_force/boj/카잉달력6064.java
@@ -7,42 +7,41 @@ public class 카잉달력6064 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int T = Integer.parseInt(br.readLine());
-        카잉달력6064 main = new 카잉달력6064();
-        while ((T--) > 0) {
+        while(T-- > 0){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int M = Integer.parseInt(st.nextToken());
             int N = Integer.parseInt(st.nextToken());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            bw.write(main.solution(M, N, x, y) + "\n");
+
+            bw.write(calculate(M,N,x,y)+"\n");
         }
 
+        br.close();
         bw.flush();
         bw.close();
-        br.close();
     }
 
-    private int solution(int M, int N, int X, int Y) {
-        int lcm = (M * N) / gcd(M, N);
-        if (Y == N) {
-            Y=0;
+    public static int calculate(int M, int N, int x, int y){
+        int lcm = M*N/gcd(M,N);
+        if(N==y){
+            y = 0;
         }
-        
-        for(int year = X; year <= lcm; year+=M){
-            if(year % N == Y){
-                return year;
+
+        for(int i = x; i<=lcm; i+=M){
+            if(i%N==y){
+                return i;
             }
         }
 
         return -1;
     }
 
-    private int gcd(int a, int b) {
-        if (a % b == 0) {
+    public static int gcd(int a, int b){
+        if(a%b == 0){
             return b;
         }
-        return gcd(b, (a % b));
+        return gcd(b,a%b);
     }
 }

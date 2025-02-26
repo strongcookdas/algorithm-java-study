e59e5ffe74ce48c8f4b8eb9a637e37b9d58f7498
strongcookdas <goodjav141592@gmail.com>
2025-02-26 15:37:11 +0900

    chore : [PG] Queue | 프로세스 | 20M

--- src/main/java/algorithm/stack_queue/programmers/프로세스.java
+++ src/main/java/algorithm/stack_queue/programmers/프로세스.java
@@ -4,22 +4,33 @@ import java.util.*;
 
 public class 프로세스 {
     public int solution(int[] priorities, int location) {
-        PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
-        Queue<Integer> q = new LinkedList<>();
+
+        List<Integer> sortedPriorities = new ArrayList<>();
+        for (int priority : priorities) {
+            sortedPriorities.add(priority);
+        }
+        sortedPriorities.sort(Collections.reverseOrder());
+
+        Queue<int[]> q = new LinkedList<>();
 
         for(int i = 0; i<priorities.length; i++){
-            q.offer(i);
-            pQ.offer(priorities[i]);
+            q.offer(new int[]{i,priorities[i]});
         }
 
-        int cnt = 1;
-        while(q.peek()!=location || pQ.peek()!=priorities[q.peek()]){
-            if(pQ.peek() == priorities[q.peek()]){
+        int cnt = 0;
+        int idx = 0;
+
+        while(!q.isEmpty()){
+            int[] tmp = q.poll();
+
+            if(tmp[1] == sortedPriorities.get(idx)){
                 cnt++;
-                pQ.poll();
-                q.poll();
+                idx++;
+                if(tmp[0] == location){
+                    return cnt;
+                }
             }else{
-                q.offer(q.poll());
+                q.offer(tmp);
             }
         }
 

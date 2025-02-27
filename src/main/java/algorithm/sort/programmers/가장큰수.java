d28373b137bed8aed3b0cf281a5a78641b8e8b12
strongcookdas <goodjav141592@gmail.com>
2025-02-27 14:53:16 +0900

    feat : [PG] 정렬 | 가장 큰 수 | 20m

--- src/main/java/algorithm/sort/programmers/가장큰수.java
+++ src/main/java/algorithm/sort/programmers/가장큰수.java
@@ -4,20 +4,26 @@ import java.util.*;
 
 public class 가장큰수 {
     public String solution(int[] numbers) {
-        String answer = "";
-        Integer[] nums = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
+        String[] numStrs = new String[numbers.length];
+        for(int i = 0; i<numbers.length; i++){
+            numStrs[i] = String.valueOf(numbers[i]);
+        }
 
-        Arrays.sort(nums, (a, b) -> {
-            String order1 = String.valueOf(a) + String.valueOf(b);
-            String order2 = String.valueOf(b) + String.valueOf(a);
-            return order2.compareTo(order1);
+        Arrays.sort(numStrs, (a,b) -> {
+            String str1 = String.valueOf(a) + String.valueOf(b);
+            String str2 = String.valueOf(b) + String.valueOf(a);
+            return str2.compareTo(str1);
         });
 
-        if (nums[0] == 0) {
+        if(Integer.valueOf(numStrs[0])==0){
             return "0";
         }
 
-        for(int number : nums) answer += String.valueOf(number);
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        for(String num : numStrs){
+            sb.append(num);
+        }
+
+        return sb.toString();
     }
 }

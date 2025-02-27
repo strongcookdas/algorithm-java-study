54c951aeea0ca521f1b0146383275b3c4acb0e31
strongcookdas <goodjav141592@gmail.com>
2025-02-27 16:34:15 +0900

    feat : [PG] 완전 탐색 | 최소 직사각형 | 30m

--- src/main/java/algorithm/brute_force/programmers/최소직사각형.java
+++ src/main/java/algorithm/brute_force/programmers/최소직사각형.java
@@ -2,41 +2,17 @@ package algorithm.brute_force.programmers;
 
 public class 최소직사각형  {
     public int solution(int[][] sizes) {
-        int n = sizes.length;
-        int widthMax = Integer.MIN_VALUE, heightMax = Integer.MIN_VALUE;
-        for(int i = 0; i<n; i++){
-            widthMax = Math.max(widthMax,sizes[i][0]);
-            heightMax = Math.max(heightMax, sizes[i][1]);
+        int rowMax = 0;
+        int colMax = 0;
+
+        for(int i = 0; i<sizes.length; i++){
+            int row = sizes[i][0];
+            int col = sizes[i][1];
+
+            rowMax = Math.max(rowMax, Math.max(row, col));
+            colMax = Math.max(colMax, Math.min(row, col));
         }
 
-        if(widthMax>heightMax){
-            heightMax = Integer.MIN_VALUE;
-            for(int i = 0; i<n; i++){
-                if(sizes[i][1]>sizes[i][0]) heightMax = Math.max(sizes[i][0],heightMax);
-                else heightMax = Math.max(sizes[i][1],heightMax);
-            }
-        }else{
-            widthMax = Integer.MIN_VALUE;
-            for(int i = 0; i<n; i++){
-                if(sizes[i][0]>sizes[i][1]) widthMax = Math.max(sizes[i][1],widthMax);
-                else widthMax = Math.max(sizes[i][0],widthMax);
-            }
-        }
-        return widthMax*heightMax;
+        return rowMax * colMax;
     }
 }
-
-/*
-이 코드가 잘 작성된 코드같다.
-class Solution {
-    public int solution(int[][] sizes) {
-        int n = sizes.length;
-        int widthMax = Integer.MIN_VALUE, heightMax = Integer.MIN_VALUE;
-        for(int i = 0; i<n; i++){
-            widthMax = Math.max(widthMax,Math.max(sizes[i][0],sizes[i][1])); // 가로 세로에서 큰 값 중 최댓값
-            heightMax = Math.max(heightMax,Math.min(sizes[i][0],sizes[i][1])); // 가로 세로에서 작은 값 중 최댓값
-        }
-        return widthMax*heightMax;
-    }
-}
- */

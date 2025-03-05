83666d327f0d39812fb41ba9d3c7f32bc7b498d4
strongcookdas <goodjav141592@gmail.com>
2025-03-05 16:32:54 +0900

    feat : [PG] 완전 탐색 | 카펫 | 20m

--- src/main/java/algorithm/brute_force/programmers/카펫.java
+++ src/main/java/algorithm/brute_force/programmers/카펫.java
@@ -3,10 +3,15 @@ package algorithm.brute_force.programmers;
 public class 카펫 {
     public int[] solution(int brown, int yellow) {
         int[] answer = {};
-        for (int i = 1; i * i <= yellow; i++) {
-            if (yellow % i == 0 && (i + (yellow / i) + 2) * 2 == brown) {
-                answer = new int[]{yellow / i + 2, i + 2};
-                return answer;
+        for(int i = 1; i*i <= yellow; i++){
+            if(yellow % i == 0){
+                int row = yellow/i;
+                int col = i;
+                int totalBrown = ((row+2)*2)+(col*2);
+                if(totalBrown == brown){
+                    answer = new int[]{row + 2, col + 2};
+                    break;
+                }
             }
         }
         return answer;

0743f390ed30a0521c3e2fbccc43d8ee9c40e197
strongcookdas <goodjav141592@gmail.com>
2025-03-05 19:17:12 +0900

    feat : [PG] 정렬 | H-Index | 30m

--- src/main/java/algorithm/sort/programmers/HIndex.java
+++ src/main/java/algorithm/sort/programmers/HIndex.java
@@ -4,15 +4,13 @@ import java.util.*;
 
 class HIndex {
     public int solution(int[] citations) {
-        int answer = 0;
-        int count = 0;
         Arrays.sort(citations);
-        for(int i = citations.length-1; i>=0; i--){
-            int citation = citations[i];
-            count++;
-            int tmp = Math.min(citation,count);
-            answer = Math.max(tmp, answer);
+        int n = citations.length;
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i) {
+                return n - i;
+            }
         }
-        return answer;
+        return 0;
     }
 }

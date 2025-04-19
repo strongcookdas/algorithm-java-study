d3cd52b6271a5f9cb6d6a9af74635274f89e61dd
strongcookdas <goodjav141592@gmail.com>
2025-04-19 23:58:33 +0900

    feat : [BOJ] bfs | 숨바꼭질3 | 13549 | 1h

--- src/main/java/algorithm/bfs/boj/숨바꼭질13549.java
+++ src/main/java/algorithm/bfs/boj/숨바꼭질13549.java
@@ -2,42 +2,63 @@ package algorithm.bfs.boj;
 
 //https://www.acmicpc.net/problem/13549
 
+import algorithm.Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class 숨바꼭질13549 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+    static class Position {
+        int x;
+        int time;
 
-        숨바꼭질13549 main = new 숨바꼭질13549();
-        System.out.println(main.solution(n, k));
+        public Position(int x, int time) {
+            this.x = x;
+            this.time = time;
+        }
     }
 
-    public int solution(int n, int k) {
-        Queue<Integer> q = new LinkedList<>();
-        int[] time = new int[100005];
-        Arrays.fill(time, -1);
-        q.offer(n);
-        time[n] = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(solution(A, B));
+    }
+
+    public static int solution(int A, int B) {
+        Queue<Position> q = new LinkedList<>();
+        boolean[] visited = new boolean[100001];
+        q.offer(new Position(A, 0));
+        visited[A] = true;
+
         while (!q.isEmpty()) {
             int size = q.size();
             for (int i = 0; i < size; i++) {
-                int now = q.poll();
-                if (now == k) {
-                    return time[now];
+                Position tmp = q.poll();
+                if (tmp.x == B) {
+                    return tmp.time;
                 }
-                if (now * 2 >= 0 && now * 2 < time.length && time[now * 2] == -1) {
-                    time[now * 2] = time[now];
-                    q.offer(now * 2);
+
+                int nextX1 = tmp.x*2;
+                int nextX2 = tmp.x-1;
+                int nextX3 = tmp.x+1;
+
+                if (nextX1 >= 0 && nextX1< visited.length && !visited[nextX1]) {
+                    q.offer(new Position(nextX1, tmp.time));
+                    visited[nextX1]=true;
                 }
-                if (now - 1 >= 0 && now - 1 < time.length && time[now - 1] == -1) {
-                    time[now - 1] = time[now] + 1;
-                    q.offer(now - 1);
+                if (nextX2 >= 0 && nextX2< visited.length && !visited[nextX2]) {
+                    q.offer(new Position(nextX2, tmp.time+1));
+                    visited[nextX2]=true;
                 }
-                if (now + 1 >= 0 && now + 1 < time.length && time[now + 1] == -1) {
-                    time[now + 1] = time[now] + 1;
-                    q.offer(now + 1);
+                if (nextX3 >= 0 && nextX3< visited.length && !visited[nextX3]) {
+                    q.offer(new Position(nextX3, tmp.time+1));
+                    visited[nextX3]=true;
                 }
             }
         }
